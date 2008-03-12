32cfdde5b523ff87e1de45a199f93c8d4cbcbd97
frantaparallel <frantaparallel@ccc6a032-7b47-0410-9fd8-1f29c74ce1d2>
2008-03-12 18:03:14 +0000

    added demonstration of using indicators
    git-svn-id: http://tradelink.googlecode.com/svn/trunk@49 ccc6a032-7b47-0410-9fd8-1f29c74ce1d2

--- Box-Examples/MAbox.cs
+++ Box-Examples/MAbox.cs
@@ -17,11 +17,20 @@ namespace box
 	/// </summary>
 	public class MAbox : TradeBox
 	{
+        // indicator identifiers
+        const int imkt = 0;
+        const int ibar = 1;
 		public MAbox(NewsService ns) : base(ns)
 		{
 			Name = "MABox";
+            // auto-money-management features provided by TradeBox
 			this.ProfitTarget = 1;
 			this.Stop = .5m;
+
+            // demo of indicator tracking (used by other tools)
+            this.ResetIndicators(2);
+            this.SetIname(imkt, "Mkt*100");
+            this.SetIname(ibar, "2BarMA*100");
 		}
 		protected override bool EnterLong()	{ return MAenter(); }
 		protected override bool EnterShort() 	{ return MAenter(); }
@@ -29,11 +38,18 @@ namespace box
 		bool MAenter()
 		{
             bool enter = false;
+
             decimal market = this.getMostRecentTrade();
             if (!this.bl.Has(2) || (market==0)) return enter;
 			decimal MA = SMA.BarSMA(this.bl,BarInterval.Minute,2);
 
+            // debug output (optional, sent if this.Debug field is true)
 			this.D("market:"+market.ToString("N2")+" MA:"+MA.ToString("N2"));
+
+            // expose indicators (optional, if used by other tools)
+            this.SetIndicators(new int[] { (int)market * 100, (int)MA * 100 });
+
+            // rules that determine how indicators lead to entry (or not)
 			if (this.Side && (market>MA)) enter = true; // long entry
 			if (!this.Side && (market<MA)) enter = true; // short entry
 			return enter;
