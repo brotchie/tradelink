9d32572eab055e080238fcf5fbede2e7bfa15da7
frantaparallel <frantaparallel@ccc6a032-7b47-0410-9fd8-1f29c74ce1d2>
2008-03-13 22:28:37 +0000

    added TickLink tests
    git-svn-id: http://tradelink.googlecode.com/svn/trunk@56 ccc6a032-7b47-0410-9fd8-1f29c74ce1d2

--- TradeLib/Link.cs
+++ TradeLib/Link.cs
@@ -11,24 +11,27 @@ namespace TradeLib
         public Tick A { get { return a; } set { a = value; } }
         public Tick B { get { return b; } set { b = value; } }
         private bool t { get { return a.isTrade; } }
-        public bool PlusTick { get { return Valid && t && (a.trade>b.trade); } }
-        public bool MinusTick { get { return Valid && t && (a.trade < b.trade); } }
-        public bool TakesOffer { get { return (Valid && (a.trade >= b.ask)); } }
-        public bool HitsBid { get { return (Valid && (a.trade <= b.bid)); } }
-        public bool Perfect { get { return (Valid && t && (a.ts >= b.os) || (a.ts >=b.bs)); } }
+        public bool PlusTick { get { return Valid && t && (_last!=0) && (_last<a.trade); } }
+        public bool MinusTick { get { return Valid && t && (_last!=0) && (_last>a.trade); } }
+        public bool TakesOffer { get { return (Valid && t && b.hasAsk && (a.trade >= b.ask)); } }
+        public bool HitsBid { get { return (Valid && t && b.hasBid && (a.trade <= b.bid)); } }
+        public bool Perfect { get { return (Valid && t && ((a.ts >= b.os) && b.hasAsk) || ((a.ts >=b.bs) && b.hasBid)); } }
         public bool TakesOfferPerfect { get { return t && Valid && TakesOffer && Perfect; } }
         public bool HitsBidPerfect { get { return t && Valid && HitsBid && Perfect; } }
-        public bool BidsOffer { get { return !t && Valid && (a.bid >= b.ask); } }
-        public bool OffersBid { get { return !t && Valid && (a.ask <= b.bid); } }
-        public bool BidsUp { get { return !t && Valid && (a.bid > b.bid); } }
-        public bool BidsDown { get { return !t && Valid && (a.bid < b.bid); } }
+        public bool BidsOffer { get { return !t && Valid && a.hasBid && b.hasAsk && (a.bid >= b.ask); } }
+        public bool OffersBid { get { return !t && Valid && a.hasAsk && b.hasBid && (a.ask <= b.bid); } }
+        public bool BidsUp { get { return !t && Valid && a.hasBid && b.hasBid && (a.bid > b.bid); } }
+        public bool BidsDown { get { return !t && Valid && a.hasBid && b.hasBid && (a.bid < b.bid); } }
         public bool OffersDown { get { return !t && Valid && (a.ask < b.ask); } }
-        public bool OffersUp { get { return !t && Valid && (a.ask > b.ask); } }
-        public bool OfferReload { get { return !t && Valid && (a.trade == b.ask) && (a.ask == b.ask); } }
-        public bool BidReload { get { return !t && Valid && (a.trade == b.bid) && (a.bid == b.bid); } }
+        public bool OffersUp { get { return !t && Valid && a.hasAsk && b.hasAsk && (a.ask > b.ask); } }
+        public bool OfferReload { get { return !t && Valid && a.hasAsk && b.hasAsk && (a.trade == b.ask) && (a.ask == b.ask); } }
+        public bool BidReload { get { return !t && Valid && a.hasBid && b.hasBid && (a.trade == b.bid) && (a.bid == b.bid); } }
         public bool Valid { get { return (a != null) && (b != null) && a.hasTick && b.hasTick; } }
+        private decimal _last;
         public virtual bool Tick(Tick tick) 
         {
+            if ((a!=null) && a.isTrade)
+                _last = a.trade;
             if (a == null) a = new Tick(tick);
             else if (!a.hasTick) a = new Tick(a, tick);
             else if (a.hasTick)
