8b327a2da36495c013a00142785ec0711cf6e446
frantaparallel <frantaparallel@ccc6a032-7b47-0410-9fd8-1f29c74ce1d2>
2008-03-20 22:18:13 +0000

    added index basket creation and counting, to mirror market basket.
    git-svn-id: http://tradelink.googlecode.com/svn/trunk@67 ccc6a032-7b47-0410-9fd8-1f29c74ce1d2

--- TradeLib/IndexBasket.cs
+++ TradeLib/IndexBasket.cs
@@ -10,11 +10,21 @@ namespace TradeLib
         public bool hasIndex { get { return l.Count > 0; } }
         public void Add(Index i) { l.Add(i); }
         public void Remove(Index i) { l.Remove(i); }
+        public Index this[int i] { get { return l[i]; } set { l[i] = value; } }
+        public int Count { get { return l.Count; } }
         public override string ToString()
         {
             string s = "";
             for (int i = 0; i < l.Count; i++) s += l[i].Name + ",";
             return s;
+        }
+        public static IndexBasket FromString(string serializedbasket)
+        {
+            IndexBasket ib = new IndexBasket();
+            string[] name = serializedbasket.Split(',');
+            for (int i = 0; i < name.Length; i++)
+                ib.Add(new Index(name[i]));
+            return ib;
         }
     }
 }
