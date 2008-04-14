577da8da062fa9f697bd6044c1c8c59b336ffce2
frantaparallel <frantaparallel@ccc6a032-7b47-0410-9fd8-1f29c74ce1d2>
2008-04-14 21:16:05 +0000

    added quoting to escape long file names in VizStudio paths... removed defined check since not needed.
    git-svn-id: http://tradelink.googlecode.com/svn/trunk@127 ccc6a032-7b47-0410-9fd8-1f29c74ce1d2

--- TradeLib/runtests.cmd
+++ TradeLib/runtests.cmd
@@ -1,6 +1,6 @@
 rem @echo off
-if NOT DEFINED NUNIT OR NOT EXIST NUNIT (
+
 for /F "delims=;" %%i in ('dir /b c:\progra~1\NUnit*') do set n_dir=%%i
 set NUNIT=c:\progra~1\%n_dir%
-)
-"%NUNIT%\bin\nunit-console.exe" %1 /config:%2
+"%NUNIT%\bin\nunit-console.exe" "%1" /config:%2
+set NUNIT=
