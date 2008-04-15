e10d8202977d6b9f0ea22586c619da7cd0fc8f82
frantaparallel <frantaparallel@ccc6a032-7b47-0410-9fd8-1f29c74ce1d2>
2008-04-15 18:55:58 +0000

    fixed open/closed size displaying with decimal.
    git-svn-id: http://tradelink.googlecode.com/svn/trunk@142 ccc6a032-7b47-0410-9fd8-1f29c74ce1d2

--- TradeLib/Util.cs
+++ TradeLib/Util.cs
@@ -181,7 +181,7 @@ namespace TradeLib
                     opl = BoxMath.OpenPL(t.xprice,posdict[s]); // get any leftover open pl
                     if (cpl != 0) csize = t.xsize; // if we closed any pl, get the size
                 }
-                string[] pl = new string[] { opl.ToString("C2"), cpl.ToString("C2"), posdict[s].Size.ToString("N2"), csize.ToString(), posdict[s].AvgPrice.ToString("N2") };
+                string[] pl = new string[] { opl.ToString("C2"), cpl.ToString("C2"), posdict[s].Size.ToString(), csize.ToString(), posdict[s].AvgPrice.ToString("N2") };
                 sw.WriteLine(string.Join(delimiter.ToString(),pl)+delimiter);
             }
             sw.Close();           
