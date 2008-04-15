9b7dc7c1f79965b4af7e19d3dbac2c4f8cad9a20
frantaparallel <frantaparallel@ccc6a032-7b47-0410-9fd8-1f29c74ce1d2>
2008-04-15 14:47:21 +0000

    moved simple parameter to first in batch file, moved long file name parater to 2nd... added long-file name parameter reference instead of normal.  tested with long file name in checkout directory (including space), and non-space checkout directory.  worked.
    this is for issue #11.
    
    git-svn-id: http://tradelink.googlecode.com/svn/trunk@140 ccc6a032-7b47-0410-9fd8-1f29c74ce1d2

--- TradeLib/runtests.cmd
+++ TradeLib/runtests.cmd
@@ -2,5 +2,5 @@ rem @echo off
 
 for /F "delims=;" %%i in ('dir /b c:\progra~1\NUnit*') do set n_dir=%%i
 set NUNIT=c:\progra~1\%n_dir%
-"%NUNIT%\bin\nunit-console.exe" "%1" /config:%2
+"%NUNIT%\bin\nunit-console.exe" "%~f2" /config:%1
 set NUNIT=
