e5720925949f81399aa76be808e480576602d9c3
frantaparallel <frantaparallel@ccc6a032-7b47-0410-9fd8-1f29c74ce1d2>
2008-04-24 22:21:59 +0000

    more alpha-quality work with testhistsim.
    git-svn-id: http://tradelink.googlecode.com/svn/trunk@174 ccc6a032-7b47-0410-9fd8-1f29c74ce1d2

--- TradeLib/HistSim.cs
+++ TradeLib/HistSim.cs
@@ -5,13 +5,18 @@ using System.IO;
 
 namespace TradeLib
 {
-    public class HistSim : TradeLinkServer
+    public class HistSim 
     {
+        public event TickDelegate GotTick;
+        public event FillDelegate GotFill;
+        public event DebugDelegate GotDebug;
+        public event IndexDelegate GotIndex;
         int _simtime = 0;
         public int NumClients { get { return 0; } }
         string _folder;
         TickFileFilter _filter = new TickFileFilter();
         bool _ticksloaded = false;
+        bool _loadindex = true;
         Broker _broker = new Broker();
         string[] _tickfiles;
         public Broker SimBroker { get { return _broker; } set { _broker = value; } }
@@ -21,6 +26,7 @@ namespace TradeLib
             _folder = TickFolder;
             if (tff != null)
                 _filter = tff;
+            _broker.
         }
 
         public void Load()
@@ -29,9 +35,49 @@ namespace TradeLib
             _tickfiles = _filter.Allows(files);
 
         }
+        private void show(string message) { if (GotDebug!=null) GotDebug(message); }
 
-        public void GoSrv() { }
-        public void newTick(Tick t) { }
-        public void newFill(Trade trade) { }
+        private Dictionary<string, List<Index>> idxstream = new Dictionary<string, List<Index>>();
+
+        private void LoadIndexFiles(int date)
+        {
+            idxstream.Clear();
+            Dictionary<string, StreamReader> idxfile = new Dictionary<string, StreamReader>();
+            DirectoryInfo di = new DirectoryInfo(_folder);
+            FileInfo[] files = di.GetFiles("*"+date+"*.idx");
+            for (int i = 0; i < files.Length; i++)
+                if (!idxfile.ContainsKey(files[i].FullName))
+                    idxfile.Add(files[i].FullName, new StreamReader(files[i].FullName));
+                else idxfile[files[i].FullName] = new StreamReader(files[i].FullName);
+            show(Environment.NewLine);
+            show("Preparing "+idxfile.Count+ " indicies: ");
+            foreach (string stock in idxfile.Keys)
+            {
+                if (!idxstream.ContainsKey(stock))
+                    idxstream.Add(stock, new List<Index>());
+                string sym = "";
+                while (!idxfile[stock].EndOfStream)
+                { // read every index into memory
+                    string line = idxfile[stock].ReadLine();
+                    Index fi = Index.Deserialize(line);
+                    idxstream[stock].Add(fi);
+                    sym = fi.Name;
+                }
+                show("Prepared "+sym+".");
+            }
+        }
+
+
+        List<Index> FetchIdx(int time)
+        {
+            List<Index> res = new List<Index>();
+            foreach (string file in idxstream.Keys)
+            {
+                for (int i = 0; i < idxstream[file].Count; i++)
+                    if (idxstream[file][i].Time == time)
+                        res.Add(idxstream[file][i]);
+            }
+            return res;
+        }
     }
 }
---
+++ TradeLib/TestHistSim.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using TradeLib;
+
+namespace TestTradeLib
+{
+    [TestFixture]
+    public class TestHistSim
+    {
+        public TestHistSim() { }
+
+        Tick[] timesales = new Tick[] 
+            {
+                Tick.NewTrade("IBM",100,100),
+                Tick.NewTrade("LVS",90,200),
+                Tick.NewQuote("LVS",20080425,931,0,90.00,90.11,10,2,"NYS","NYS"),
+                Tick.NewTrade("LVS",90.11,100),
+                Tick.NewTrade("IBM",100,200),
+            };
+
+        [Test]
+        public void Basics()
+        {
+            HistSim h = new HistSim(new TickFileFilter(new string[] { "IBM" }));
+            h.
+        }
+    }
+}
