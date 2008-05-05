a46b602a8cf91c1b8a6fda0d0c43d3d9a51c5938
chrisheidbrink@yahoo.com <chrisheidbrink@yahoo.com@ccc6a032-7b47-0410-9fd8-1f29c74ce1d2>
2008-05-05 23:21:28 +0000

    repeat:
    should be same add as Added Bollinger.cs added TestBollinger.cs, had to add nunit.framework to the Box-Examples references and remove/add nunit.framework from the TestTradeLib references
    
    
    git-svn-id: http://tradelink.googlecode.com/svn/trunk@190 ccc6a032-7b47-0410-9fd8-1f29c74ce1d2

---
+++ Box-Examples/Bollinger.cs
@@ -0,0 +1,138 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TradeLib;
+
+namespace Atlas
+{
+    public class Bollinger
+    {
+        private int sds;
+        private int lookback;
+        private BarInterval bi;
+        private decimal count = 0;
+        private decimal sum = 0;
+        private decimal devavg = 0;
+        private double sd;
+        private decimal ub;
+        private decimal lb;
+        private decimal mean = 0;
+        private decimal sumsqtrade = 0;
+        bool isbarcons = false;
+        List<decimal> l = new List<decimal>();
+        
+        
+        public Bollinger(int sds, BarInterval bi, int lookback)
+        { this.sds = sds;
+        this.bi = bi;
+        this.lookback = lookback;
+        isbarcons = true;
+        }
+
+
+        
+        public Bollinger(int sds, int lookback)
+        {
+            this.sds = sds;
+            this.lookback = lookback;
+            isbarcons = false;
+        }
+
+        public Bollinger() : this(2,10) { }
+
+        public void newTick  ( Tick t )
+        {
+            if (isbarcons) throw new Exception("You can't call newTick method without using the right constructor");
+                if (t.isTrade)
+                {
+                    count++;
+                    l.Add(t.trade);
+
+                    if (l.Count > lookback)
+                    {
+                        l.RemoveAt(0);
+                        count--;
+                    }
+
+                    sum = 0;
+                    sumsqtrade = 0;
+
+                    foreach (decimal v in l)
+                    {
+                        sum += v;
+                        sumsqtrade = sumsqtrade + v * v;
+                    }
+
+
+                    mean = sum / count;
+                    devavg = (sumsqtrade - sum * sum / count) / count;
+                }
+                sd = Math.Sqrt((double)devavg);
+                ub = mean + sds * (decimal)sd;
+                lb = mean - sds * (decimal)sd;
+        }
+
+        public bool hasLookbacks
+        { get { return (l.Count == lookback); } }
+        
+        public void newBar(BarList bl)
+        {
+            Bar obar = bl.Get(bl.BarZero);
+            if (!isbarcons) throw new Exception("You can't call a newBar method without using the right constructor.");
+            if (bl.NewBar)
+                {
+                    count++;
+                    decimal open = obar.Open;
+
+                    l.Add(open);
+
+                    if (l.Count > lookback)
+                    {
+                        l.RemoveAt(0);
+                        count--;
+                    }
+
+                    sum = 0;
+                    sumsqtrade = 0;
+                    foreach (decimal v in l)
+                    {
+                        sum += v;
+                        sumsqtrade = sumsqtrade + v * v;
+                    }
+                    mean = sum / count;
+                    devavg = (sumsqtrade - sum * sum / count) / count;
+                }
+
+
+                sd = Math.Sqrt((double)devavg);
+                ub = mean + sds * (decimal)sd;
+                lb = mean - sds * (decimal)sd;
+        }
+
+
+
+        public decimal Mean
+        { get { return mean; }}
+
+        public decimal Devavg
+        { get { return devavg; } }
+
+        public double Sd
+        { get { return sd; }}
+
+        public decimal Upperband
+        { get { return ub; } }
+
+        public decimal Lowerband
+        { get { return lb; } }
+
+        public void Reset()
+        {
+            sum = 0;
+            mean = 0;
+            count = 0;
+            sumsqtrade = 0;
+            devavg = 0;
+        }
+    }
+}
---
+++ Box-Examples/TestBollinger.cs
@@ -0,0 +1,143 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TradeLib;
+using NUnit.Framework;
+
+
+namespace Atlas
+{
+    [TestFixture]
+    public class TestBollinger
+    {
+        public TestBollinger()
+        {
+        }
+        const string s = "TST";
+        const string x = "NYS";
+        const int d = 20070917;
+        const int t = 929;
+
+        // we test 2 standard deviations here
+
+
+        Tick[] ticklist = new Tick[] {
+            Tick.NewTrade(s,d,t,0,1,100,x),
+            Tick.NewTrade(s,d,t+1,0,2,100,x),
+            Tick.NewTrade(s,d,t+2,0,3,100,x),
+            Tick.NewTrade(s,d,t+3,0,4,100,x),
+            Tick.NewTrade(s,d,t+4,0,5,100,x),
+        };
+
+        [Test]
+        public void BollingerTestticklookbacks()
+        {
+            Bollinger bbbb = new Bollinger(2, 4);
+            foreach (Tick t in ticklist)
+            {
+                bbbb.newTick(t);
+            }
+
+
+            Assert.That(bbbb.Mean == 3.5M, bbbb.Mean.ToString());
+            Assert.That(bbbb.Devavg == 1.25M, bbbb.Devavg.ToString());
+            Assert.That(bbbb.Sd == 1.1180339887498948482045868343656, bbbb.Sd.ToString());
+            Assert.That(bbbb.Upperband == 5.73606797749978M, bbbb.Upperband.ToString());
+            Assert.That(bbbb.Lowerband == 1.26393202250022M, bbbb.Lowerband.ToString());
+        }
+        
+        
+        
+        [Test]
+        public void BollingerTesttick()
+        {
+            Bollinger b = new Bollinger(2, 10);
+
+            foreach (Tick t in ticklist)
+            {
+                b.newTick(t);
+            }
+
+
+            Assert.That(b.Mean == 3, b.Mean.ToString());
+            Assert.That(b.Devavg == 2, b.Devavg.ToString());
+            Assert.That(b.Sd == 1.4142135623730950488016887242097, b.Sd.ToString());
+            Assert.That(b.Upperband == 5.8284271247462M, b.Upperband.ToString());
+            Assert.That(b.Lowerband == 0.1715728752538M, b.Lowerband.ToString());
+        }
+
+
+        [Test]
+        public void BollingerTestticknoargs()
+        {
+            Bollinger bb = new Bollinger();
+            foreach (Tick t in ticklist)
+            {
+                bb.newTick(t);
+            }
+
+
+            Assert.That(bb.Mean == 3, bb.Mean.ToString());
+            Assert.That(bb.Devavg == 2, bb.Devavg.ToString());
+            Assert.That(bb.Sd == 1.4142135623730950488016887242097, bb.Sd.ToString());
+            Assert.That(bb.Upperband == 5.8284271247462M, bb.Upperband.ToString());
+            Assert.That(bb.Lowerband == 0.1715728752538M, bb.Lowerband.ToString());
+        }
+
+        [Test]
+
+        public void BollingerBarlisttest()
+        {
+
+            string symbol = "TST";
+
+
+            BarList bl = new BarList(BarInterval.Minute, symbol);
+            Bollinger bbb = new Bollinger(2, BarInterval.Minute, 10);
+            foreach (Tick t in ticklist)
+            {
+                bl.AddTick(t);
+                Console.WriteLine(bbb.Sd + " " + bbb.Upperband + " " + bbb.Lowerband);
+                bbb.newBar(bl);
+            }
+
+            Assert.That(bbb.Mean == 3, bbb.Mean.ToString());
+            Assert.That(bbb.Devavg == 2, bbb.Devavg.ToString());
+            Assert.That(bbb.Sd == 1.4142135623730950488016887242097, bbb.Sd.ToString());
+            Assert.That(bbb.Upperband == 5.8284271247462M, bbb.Upperband.ToString());
+            // Assert.That(bbb.Lowerband == 0.1715728752538M, bbb.Lowerband.ToString());
+        }
+        [Test]
+        public void BollingerBarlisttestlookbacks()
+        {
+            string symbol = "TST";
+
+            BarList bl = new BarList(BarInterval.Minute, symbol);
+            Bollinger bbb = new Bollinger(2, BarInterval.Minute, 4);
+            foreach (Tick t in ticklist)
+            {
+                bl.AddTick(t);
+                Console.WriteLine(bbb.Sd + " " + bbb.Upperband + " " + bbb.Lowerband);
+                bbb.newBar(bl);
+            }
+
+            Assert.That(bbb.Mean == 3.5M, bbb.Mean.ToString());
+            Assert.That(bbb.Devavg == 1.25M, bbb.Devavg.ToString());
+            Assert.That(bbb.Sd == 1.1180339887498948482045868343656, bbb.Sd.ToString());
+            Assert.That(bbb.Upperband == 5.73606797749978M, bbb.Upperband.ToString());
+            Assert.That(bbb.Lowerband == 1.26393202250022M, bbb.Lowerband.ToString());
+        }
+    
+    
+    }
+        
+}
+
+
+
+
+
+
+
+
+
