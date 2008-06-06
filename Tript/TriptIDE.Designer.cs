7efc2f06419b3e0e667202e89e79da1f78dde583
frantaparallel <frantaparallel@ccc6a032-7b47-0410-9fd8-1f29c74ce1d2>
2008-06-06 20:30:10 +0000

    added save/saveas/new/load buttons to tript.  added f5 hotkey for run button.
    git-svn-id: http://tradelink.googlecode.com/svn/trunk@248 ccc6a032-7b47-0410-9fd8-1f29c74ce1d2

--- Tript/TriptIDE.Designer.cs
+++ Tript/TriptIDE.Designer.cs
@@ -32,6 +32,10 @@ namespace Tript
             this.editorbox = new System.Windows.Forms.RichTextBox();
             this.outbox = new System.Windows.Forms.RichTextBox();
             this.runbut = new System.Windows.Forms.Button();
+            this.loadbut = new System.Windows.Forms.Button();
+            this.savebut = new System.Windows.Forms.Button();
+            this.saveas = new System.Windows.Forms.Button();
+            this.newbut = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // editorbox
@@ -39,34 +43,79 @@ namespace Tript
             this.editorbox.AcceptsTab = true;
             this.editorbox.Location = new System.Drawing.Point(13, 13);
             this.editorbox.Name = "editorbox";
-            this.editorbox.Size = new System.Drawing.Size(457, 207);
+            this.editorbox.Size = new System.Drawing.Size(516, 216);
             this.editorbox.TabIndex = 0;
             this.editorbox.Text = "";
+            this.editorbox.KeyUp += new System.Windows.Forms.KeyEventHandler(this.editorbox_KeyUp);
             // 
             // outbox
             // 
-            this.outbox.Location = new System.Drawing.Point(13, 280);
+            this.outbox.Location = new System.Drawing.Point(13, 264);
             this.outbox.Name = "outbox";
             this.outbox.ReadOnly = true;
-            this.outbox.Size = new System.Drawing.Size(570, 85);
+            this.outbox.Size = new System.Drawing.Size(570, 101);
             this.outbox.TabIndex = 1;
             this.outbox.Text = "";
             // 
             // runbut
             // 
-            this.runbut.Location = new System.Drawing.Point(477, 13);
+            this.runbut.Location = new System.Drawing.Point(13, 235);
             this.runbut.Name = "runbut";
-            this.runbut.Size = new System.Drawing.Size(75, 23);
+            this.runbut.Size = new System.Drawing.Size(48, 23);
             this.runbut.TabIndex = 2;
             this.runbut.Text = "Run";
             this.runbut.UseVisualStyleBackColor = true;
             this.runbut.Click += new System.EventHandler(this.runbut_Click);
+            // 
+            // loadbut
+            // 
+            this.loadbut.Location = new System.Drawing.Point(535, 99);
+            this.loadbut.Name = "loadbut";
+            this.loadbut.Size = new System.Drawing.Size(51, 23);
+            this.loadbut.TabIndex = 3;
+            this.loadbut.Text = "Load";
+            this.loadbut.UseVisualStyleBackColor = true;
+            this.loadbut.Click += new System.EventHandler(this.loadbut_Click);
+            // 
+            // savebut
+            // 
+            this.savebut.Location = new System.Drawing.Point(535, 41);
+            this.savebut.Name = "savebut";
+            this.savebut.Size = new System.Drawing.Size(48, 23);
+            this.savebut.TabIndex = 4;
+            this.savebut.Text = "Save";
+            this.savebut.UseVisualStyleBackColor = true;
+            this.savebut.Click += new System.EventHandler(this.savebut_Click);
+            // 
+            // saveas
+            // 
+            this.saveas.Location = new System.Drawing.Point(535, 70);
+            this.saveas.Name = "saveas";
+            this.saveas.Size = new System.Drawing.Size(51, 23);
+            this.saveas.TabIndex = 5;
+            this.saveas.Text = "As";
+            this.saveas.UseVisualStyleBackColor = true;
+            this.saveas.Click += new System.EventHandler(this.saveas_Click);
+            // 
+            // newbut
+            // 
+            this.newbut.Location = new System.Drawing.Point(535, 12);
+            this.newbut.Name = "newbut";
+            this.newbut.Size = new System.Drawing.Size(48, 23);
+            this.newbut.TabIndex = 6;
+            this.newbut.Text = "New";
+            this.newbut.UseVisualStyleBackColor = true;
+            this.newbut.Click += new System.EventHandler(this.newbut_Click);
             // 
             // TriptIDE
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(595, 377);
+            this.Controls.Add(this.newbut);
+            this.Controls.Add(this.saveas);
+            this.Controls.Add(this.savebut);
+            this.Controls.Add(this.loadbut);
             this.Controls.Add(this.runbut);
             this.Controls.Add(this.outbox);
             this.Controls.Add(this.editorbox);
@@ -82,6 +131,10 @@ namespace Tript
         private System.Windows.Forms.RichTextBox editorbox;
         private System.Windows.Forms.RichTextBox outbox;
         private System.Windows.Forms.Button runbut;
+        private System.Windows.Forms.Button loadbut;
+        private System.Windows.Forms.Button savebut;
+        private System.Windows.Forms.Button saveas;
+        private System.Windows.Forms.Button newbut;
     }
 }
 
--- Tript/TriptIDE.cs
+++ Tript/TriptIDE.cs
@@ -19,6 +19,12 @@ namespace Tript
             InitializeComponent();
             _redirect = new TextBoxWriter(outbox); 
             Console.SetOut(_redirect); // redirect console writes to output textbox in tript
+            editorbox.TextChanged +=new EventHandler(editorbox_TextChanged);
+        }
+
+        void  editorbox_TextChanged(object sender, EventArgs e)
+        {
+            hasmodfiication = true;
         }
 
         void output(string outputtext)
@@ -29,6 +35,7 @@ namespace Tript
                 outbox.AppendText(outputtext);
         }
 
+        void run() { runbut_Click(null, null); }
         private void runbut_Click(object sender, EventArgs e)
         {
             outbox.Clear();
@@ -37,5 +44,74 @@ namespace Tript
             // run the script using the stream
             CSScriptCompiler.RunScript(_workingcopy, new string[] { }); // run stream
         }
+        string filename = "";
+        bool hasmodfiication = false;
+
+        private void loadbut_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog od = new OpenFileDialog();
+            od.DefaultExt = ".txt";
+            od.Filter = "Tript Text Files|*.txt";
+            od.Multiselect = false;
+            if (od.ShowDialog() == DialogResult.OK)
+            {
+                editorbox.Clear();
+                editorbox.LoadFile(od.FileName,RichTextBoxStreamType.PlainText);
+                filename = od.FileName;
+                hasmodfiication = false;
+            }
+        }
+
+        private void newbut_Click(object sender, EventArgs e)
+        {
+            if (hasmodfiication)
+            {
+                DialogResult r = MessageBox.Show("Do you want to save changes to UNSAVED tript?","Config",MessageBoxButtons.YesNoCancel);
+                if (r== DialogResult.Yes)
+                    savebut_Click(null,null);
+                else if (r== DialogResult.No)
+                {
+                    filename = "";
+                    editorbox.Clear();
+                    hasmodfiication = false;
+                }
+                else if (r== DialogResult.Cancel)
+                    return;
+            }
+            filename="";
+            editorbox.Clear();
+            hasmodfiication = false;
+        }
+        private void savebut_Click(object sender, EventArgs e)
+        {
+            if (filename == "")
+                saveas_Click(null, null);
+            if (filename != "")
+            {
+                editorbox.SaveFile(filename, RichTextBoxStreamType.PlainText);
+                hasmodfiication = false;
+            }
+        }
+
+        private void saveas_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sd = new SaveFileDialog();
+            sd.AddExtension = true;
+            sd.DefaultExt = ".txt";
+            sd.Filter = "Tript Text Files|*.txt";
+            if (sd.ShowDialog() == DialogResult.OK)
+            {
+                filename = sd.FileName;
+            }
+
+        }
+
+        private void editorbox_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.F5)
+                run();
+        }
     }
+
+
 }
