156fa2ced46f37692884257e8d6e53ce6d22cf71
frantaparallel <frantaparallel@ccc6a032-7b47-0410-9fd8-1f29c74ce1d2>
2008-06-09 20:24:39 +0000

    this is associated with previous change, refactoring TradeLink_WM.cs into seperate client/server classes.
    git-svn-id: http://tradelink.googlecode.com/svn/trunk@258 ccc6a032-7b47-0410-9fd8-1f29c74ce1d2

---
+++ TradeLib/TradeLink_Client_WM.cs
@@ -0,0 +1,378 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+
+namespace TradeLib
+{
+    public class TradeLink_Client_WM : Form , TradeLinkClient
+    {
+        const string myver = "2.1";
+        const string build = "$Rev: 992 $";
+        public string Ver { get { return myver + "." + Util.CleanVer(build); } }
+        // clients that want notifications for subscribed stocks can override these methods
+        /// <summary>
+        /// Occurs when TradeLink receives any type of message [got message].
+        /// </summary>
+        public event TickDelegate gotTick;
+        public event FillDelegate gotFill;
+        public event IndexDelegate gotIndexTick;
+        public event OrderDelegate gotOrder;
+        public event DebugDelegate gotAccounts;
+        public event IntDelegate gotOrderCancel;
+
+        public TradeLink_Client_WM() : this("TradeLinkClient",true) { }
+
+        public TradeLink_Client_WM(string clientname, bool showarmingonmissingserver)
+            : base()
+        {
+            this.Text = WMUtil.GetUniqueWindow(clientname);
+            this.WindowState = FormWindowState.Minimized;
+            this.ShowInTaskbar = false;
+            this.Hide();
+            this.Mode(this.TLFound(), false, showarmingonmissingserver);
+        }
+
+        string _servername = WMUtil.SIMWINDOW;
+
+        /// <summary>
+        /// Gets or sets the other side of the link.  Him is a string that indicates the window name of the other guy.
+        /// </summary>
+        /// <value>His windowname.</value>
+        public string Him
+        {
+            get
+            {
+                return _servername;
+            }
+            set
+            {
+                _servername = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets my handle of the parent application or form.
+        /// </summary>
+        /// <value>Me H.</value>
+        public IntPtr MeH { get { return this.Handle; } }
+
+        /// <summary>
+        /// Sets the preferred communication channel of the link, if multiple channels are avaialble.
+        /// </summary>
+        /// <param name="mode">The mode.</param>
+        /// <returns></returns>
+        public bool Mode(TLTypes mode) { return Mode(mode, false, true); }
+        public bool Mode(TLTypes mode, bool showarning) { return Mode(mode, false, showarning); }
+        public bool Mode(TLTypes mode, bool throwexceptions, bool showwarning)
+        {
+            bool HandleExceptions = !throwexceptions;
+            switch (mode)
+            {
+                case TLTypes.LIVEBROKER:
+                    if (HandleExceptions)
+                    {
+                        try
+                        {
+                            GoLive();
+                            return true;
+                        }
+                        catch (TLServerNotFound)
+                        {
+
+                            if (showwarning)
+                                System.Windows.Forms.MessageBox.Show("No Live broker instance was found.  Make sure broker application + TradeLink server is running.", "TradeLink server not found");
+                            return false;
+                        }
+                    }
+                    else GoLive();
+                    break;
+                case TLTypes.SIMBROKER:
+                    if (HandleExceptions)
+                    {
+                        try
+                        {
+
+                            GoSim();
+                            return true;
+                        }
+                        catch (TLServerNotFound)
+                        {
+                            if (showwarning)
+                                System.Windows.Forms.MessageBox.Show("No simulation broker instance was found.  Make sure broker application + TradeLink server is running.", "TradeLink server not found");
+                            return false;
+                        }
+                    }
+                    else GoSim();
+                    return true;
+                    break;
+                default:
+                    if (showwarning) 
+                        System.Windows.Forms.MessageBox.Show("No simulation broker instance was found.  Make sure broker application + TradeLink server is running.", "TradeLink server not found");
+                    break;
+            }
+            return false;
+        }
+
+        public bool LiveServer = false;
+
+        /// <summary>
+        /// Makes TL client use Broker LIVE server (Broker must be logged in and TradeLink loaded)
+        /// </summary>
+        public void GoLive() { Disconnect(); himh = WMUtil.HisHandle(WMUtil.LIVEWINDOW); LiveServer = true; Register(); }
+
+        /// <summary>
+        /// Makes TL client use Broker Simulation mode (Broker must be logged in and TradeLink loaded)
+        /// </summary>
+        public void GoSim() { Disconnect(); himh = WMUtil.HisHandle(WMUtil.SIMWINDOW); LiveServer = false; Register(); }
+
+        IntPtr himh = IntPtr.Zero;
+        protected long TLSend(TL2 type) { return TLSend(type, ""); }
+        protected long TLSend(TL2 type, string m)
+        {
+            if (himh == IntPtr.Zero) throw new TLServerNotFound();
+            long res = WMUtil.SendMsg(m, himh, Handle, (int)type);
+            return res;
+        }
+        /// <summary>
+        /// Sends the order.
+        /// </summary>
+        /// <param name="o">The oorder</param>
+        /// <returns>Zero if succeeded, Broker error code otherwise.</returns>
+        public int SendOrder(Order o)
+        {
+            if (o == null) return (int)TL2.GOTNULLORDER;
+            if (!o.isValid) return (int)TL2.OK;
+            string m = o.Serialize();
+            return (int)TLSend(TL2.SENDORDER, m);
+        }
+
+        Dictionary<string, decimal> chighs = new Dictionary<string, decimal>();
+        Dictionary<string, decimal> clows = new Dictionary<string, decimal>();
+        Dictionary<string, Position> cpos = new Dictionary<string, Position>();
+
+        /// <summary>
+        /// Today's high
+        /// </summary>
+        /// <param name="sym">The symbol.</param>
+        /// <returns></returns>
+        public decimal DayHigh(string sym) { return WMUtil.unpack(TLSend(TL2.NDAYHIGH, sym)); }
+        public decimal FastHigh(string sym)
+        {
+            try
+            {
+                return chighs[sym];
+            }
+            catch (KeyNotFoundException)
+            {
+                decimal high = DayHigh(sym);
+                if (high != 0) chighs[sym] = high;
+                return high;
+            }
+        }
+        /// <summary>
+        /// Today's low
+        /// </summary>
+        /// <param name="sym">The symbol</param>
+        /// <returns></returns>
+        public decimal DayLow(string sym) { return WMUtil.unpack(TLSend(TL2.NDAYLOW, sym)); }
+        public decimal FastLow(string sym)
+        {
+            try
+            {
+                return clows[sym];
+            }
+            catch (KeyNotFoundException)
+            {
+                decimal low = DayLow(sym);
+                if (low != 0) clows[sym] = low;
+                return low;
+            }
+        }
+        /// <summary>
+        /// Today's closing price (zero if still open)
+        /// </summary>
+        /// <param name="sym">The symbol</param>
+        /// <returns></returns>
+        public decimal DayClose(string sym) { return WMUtil.unpack(TLSend(TL2.CLOSEPRICE, sym)); }
+        /// <summary>
+        /// yesterday's closing price (day)
+        /// </summary>
+        /// <param name="sym">the symbol</param>
+        /// <returns></returns>
+        public decimal YestClose(string sym) { return WMUtil.unpack(TLSend(TL2.YESTCLOSE, sym)); }
+        /// <summary>
+        /// Gets opening price for this day
+        /// </summary>
+        /// <param name="sym">The symbol</param>
+        /// <returns>decimal</returns>
+        public decimal DayOpen(string sym) { return WMUtil.unpack(TLSend(TL2.OPENPRICE, sym)); }
+        /// <summary>
+        /// Gets position size
+        /// </summary>
+        /// <param name="sym">The symbol</param>
+        /// <returns>signed integer representing position size in shares</returns>
+        public int PosSize(string sym) { return (int)TLSend(TL2.GETSIZE, sym); }
+        /// <summary>
+        /// Returns average price for a position
+        /// </summary>
+        /// <param name="sym">The symbol</param>
+        /// <returns>decimal representing average price</returns>
+        public decimal AvgPrice(string sym) { return WMUtil.unpack(TLSend(TL2.AVGPRICE, sym)); }
+        /// <summary>
+        /// Request an order be canceled
+        /// </summary>
+        /// <param name="orderid">the id of the order being canceled</param>
+        public void CancelOrder(Int64 orderid) { TLSend(TL2.ORDERCANCELREQUEST, orderid.ToString()); }
+
+        /// <summary>
+        /// Send an account request, response is returned via the gotAccounts event.
+        /// </summary>
+        /// <returns>error code, and list of accounts via the gotAccounts event.</returns>
+        /// 
+        public int RequestAccounts() { return (int)TLSend(TL2.ACCOUNTREQUEST, Text); }
+
+
+
+        public Brokers BrokerName 
+        { 
+            get 
+            { 
+                long res = TLSend(TL2.BROKERNAME);
+                return (Brokers)res;
+            } 
+        }
+
+        public Position FastPos(string sym)
+        {
+            try
+            {
+                return cpos[sym];
+            }
+            catch (KeyNotFoundException)
+            {
+                Position p = new Position(sym, AvgPrice(sym), PosSize(sym));
+                cpos.Add(sym, p);
+                return p;
+            }
+        }
+
+        public void Disconnect()
+        {
+            try
+            {
+                TLSend(TL2.CLEARCLIENT, Text);
+            }
+            catch (TLServerNotFound) { }
+        }
+
+        public void Register()
+        {
+            TLSend(TL2.REGISTERCLIENT, Text);
+        }
+
+        public void Subscribe(MarketBasket mb)
+        {
+            TLSend(TL2.REGISTERSTOCK, Text + "+" + mb.ToString());
+        }
+
+        public void RegIndex(IndexBasket ib)
+        {
+            TLSend(TL2.REGISTERINDEX, Text + "+" + ib.ToString());
+        }
+
+        public void Unsubscribe()
+        {
+            TLSend(TL2.CLEARSTOCKS, Text);
+        }
+
+        public int HeartBeat()
+        {
+            return (int)TLSend(TL2.HEARTBEAT, Text);
+        }
+
+
+
+        public TLTypes TLFound()
+        {
+            TLTypes f = TLTypes.NONE;
+            if (WMUtil.Found(WMUtil.SIMWINDOW)) f |= TLTypes.SIMBROKER;
+            if (WMUtil.Found(WMUtil.LIVEWINDOW)) f |= TLTypes.LIVEBROKER;
+            return f;
+        }
+
+
+        protected override void WndProc(ref System.Windows.Forms.Message m)
+        {
+            long result = 0;
+            TradeLinkMessage tlm = WMUtil.ToTradeLinkMessage(ref m);
+            if (tlm == null)// if it's not a WM_COPYDATA message 
+            {
+                base.WndProc(ref m); // let form process it
+                return; // we're done
+            }
+
+            string msg = tlm.body;
+            string[] r = msg.Split(',');
+            switch (tlm.type)
+            {
+                case TL2.ORDERCANCELRESPONSE:
+                    if (gotOrderCancel != null) gotOrderCancel(Convert.ToInt64(msg));
+                    break;
+                case TL2.TICKNOTIFY:
+                    if (Index.isIdx(r[(int)TickField.symbol]))
+                    {
+                        // we got an index update
+                        Index i = Index.Deserialize(msg);
+                        if (gotIndexTick != null) gotIndexTick(i);
+                        break;
+                    }
+                    Tick t = Tick.Deserialize(msg);
+                    if (t.isTrade)
+                    {
+                        try
+                        {
+                            if (t.trade > chighs[t.sym]) chighs[t.sym] = t.trade;
+                            if (t.trade < clows[t.sym]) clows[t.sym] = t.trade;
+                        }
+                        catch (KeyNotFoundException)
+                        {
+                            decimal high = DayHigh(t.sym);
+                            decimal low = DayLow(t.sym);
+                            chighs.Add(t.sym, high);
+                            if (low == 0) low = 640000;
+                            clows.Add(t.sym, low);
+                        }
+                    }
+                    if (gotTick != null) gotTick(t);
+                    break;
+                case TL2.EXECUTENOTIFY:
+                    // date,time,symbol,side,size,price,comment
+                    Trade tr = Trade.Deserialize(msg);
+                    try
+                    {
+                        cpos[tr.symbol] = new Position(tr.symbol, AvgPrice(tr.symbol), PosSize(tr.symbol));
+                    }
+                    catch (KeyNotFoundException)
+                    {
+                        cpos.Add(tr.symbol, new Position(tr.symbol, AvgPrice(tr.symbol), PosSize(tr.symbol)));
+                    }
+                    if (gotFill != null) gotFill(tr);
+                    break;
+                case TL2.ORDERNOTIFY:
+                    Order o = Order.Deserialize(msg);
+                    if (gotOrder != null) gotOrder(o);
+                    break;
+
+                case TL2.ACCOUNTRESPONSE:
+                    if (gotAccounts != null) gotAccounts(msg);
+                    break;
+            }
+            result = 0;
+            m.Result = (IntPtr)result;
+        }
+    }
+
+
+}
---
+++ TradeLib/TradeLink_Server_WM.cs
@@ -0,0 +1,222 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TradeLib
+{
+    public class TradeLink_Server_WM : Form, TradeLinkServer
+    {
+        public TradeLink_Server_WM() : this(false) { }
+        public TradeLink_Server_WM(string servername) : base()
+        {
+            this.Text = servername;
+            this.WindowState = FormWindowState.Minimized;
+            this.Show();
+            this.ShowInTaskbar = false;
+            this.Hide();
+        }
+        public TradeLink_Server_WM(bool liveserver) : this(liveserver ? WMUtil.LIVEWINDOW : WMUtil.SIMWINDOW) { }
+
+        public event OrderDelegate gotSrvFillRequest;
+        Dictionary<string, decimal> chighs = new Dictionary<string, decimal>();
+        Dictionary<string, decimal> clows = new Dictionary<string, decimal>();
+        Dictionary<string, Position> cpos = new Dictionary<string, Position>();
+
+        private void SrvDoExecute(string msg)
+        {
+            Order o = Order.Deserialize(msg);
+            if (gotSrvFillRequest != null) gotSrvFillRequest(o);
+            for (int i = 0; i < client.Count; i++)
+                WMUtil.SendMsg(msg, TL2.ORDERNOTIFY, Handle,client[i]);
+        }
+
+        public void newIndexTick(Index itick)
+        {
+            if (itick.Name == "") return;
+            for (int i = 0; i < index.Count; i++)
+                if ((client[i] != null) && (index[i].Contains(itick.Name)))
+                    WMUtil.SendMsg(Index.Serialize(itick), TL2.TICKNOTIFY, Handle,client[i]);
+        }
+
+        // server to clients
+        /// <summary>
+        /// Notifies subscribed clients of a new tick.
+        /// </summary>
+        /// <param name="tick">The tick to include in the notification.</param>
+        public void newTick(Tick tick)
+        {
+            if (tick.sym == "") return; // can't process symbol-less ticks
+            if (tick.isTrade)
+            {
+                if (!highs.ContainsKey(tick.sym)) { highs.Add(tick.sym, tick.trade); lows.Add(tick.sym, tick.trade); }
+                if (tick.trade > highs[tick.sym]) highs[tick.sym] = tick.trade;
+                if (tick.trade < lows[tick.sym]) lows[tick.sym] = tick.trade;
+            }
+
+            for (int i = 0; i < client.Count; i++) // send tick to each client that has subscribed to tick's stock
+                if ((client[i] != null) && (stocks[i].Contains(tick.sym)))
+                    WMUtil.SendMsg(tick.Serialize(), TL2.TICKNOTIFY, Handle,client[i]);
+        }
+
+        Dictionary<string, Position> SrvPos = new Dictionary<string, Position>();
+
+        /// <summary>
+        /// Notifies subscribed clients of a new execution.
+        /// </summary>
+        /// <param name="trade">The trade to include in the notification.</param>
+        public void newFill(Trade trade)
+        {
+            if (trade.symbol == "") return; // can't process symbol-less trades
+            if (!SrvPos.ContainsKey(trade.symbol)) SrvPos.Add(trade.symbol, new Position(trade));
+            else SrvPos[trade.symbol].Adjust(trade);
+            for (int i = 0; i < client.Count; i++) // send tick to each client that has subscribed to tick's stock
+                if ((client[i] != null) && (stocks[i].Contains(trade.symbol)))
+                {
+                    string msg = trade.Serialize();
+                    WMUtil.SendMsg(msg, TL2.EXECUTENOTIFY, Handle,client[i]);
+                }
+        }
+
+        public int NumClients { get { return client.Count; } }
+
+        // server structures
+
+        private Dictionary<string, decimal> lows = new Dictionary<string, decimal>();
+        private Dictionary<string, decimal> highs = new Dictionary<string, decimal>();
+        private List<string> client = new List<string>();
+        private List<DateTime> heart = new List<DateTime>();
+        private List<string> stocks = new List<string>();
+        private List<string> index = new List<string>();
+
+        private string SrvStocks(string him)
+        {
+            int cid = client.IndexOf(him);
+            if (cid == -1) return ""; // client not registered
+            return stocks[cid];
+        }
+        private string[] SrvGetClients() { return client.ToArray(); }
+        void SrvRegIndex(string cname, string idxlist)
+        {
+            int cid = client.IndexOf(cname);
+            if (cid == -1) return;
+            else index[cid] = idxlist;
+            SrvBeatHeart(cname);
+        }
+
+        void SrvRegClient(string cname)
+        {
+            if (client.IndexOf(cname) != -1) return; // already registered
+            client.Add(cname);
+            heart.Add(DateTime.Now);
+            stocks.Add("");
+            index.Add("");
+            SrvBeatHeart(cname);
+        }
+        void SrvRegStocks(string cname, string stklist)
+        {
+            int cid = client.IndexOf(cname);
+            if (cid == -1) return;
+            stocks[cid] = stklist;
+            SrvBeatHeart(cname);
+        }
+
+        void SrvClearStocks(string cname)
+        {
+            int cid = client.IndexOf(cname);
+            if (cid == -1) return;
+            stocks[cid] = "";
+            SrvBeatHeart(cname);
+        }
+        void SrvClearIdx(string cname)
+        {
+            int cid = client.IndexOf(cname);
+            if (cid == -1) return;
+            index[cid] = "";
+            SrvBeatHeart(cname);
+        }
+
+
+        void SrvClearClient(string him)
+        {
+            int cid = client.IndexOf(him);
+            if (cid == -1) return; // don't have this client to clear him
+            client.RemoveAt(cid);
+            stocks.RemoveAt(cid);
+            heart.RemoveAt(cid);
+            index.RemoveAt(cid);
+        }
+
+        void SrvBeatHeart(string cname)
+        {
+            int cid = client.IndexOf(cname);
+            if (cid == -1) return; // this client isn't registered, ignore
+            TimeSpan since = DateTime.Now.Subtract(heart[cid]);
+            heart[cid] = DateTime.Now;
+        }
+
+
+        protected override void WndProc(ref Message m)
+        {
+            TradeLinkMessage tlm = WMUtil.ToTradeLinkMessage(ref m);
+            if (tlm == null)
+            {
+                base.WndProc(ref m);
+                return;
+            }
+            string msg = tlm.body;
+
+            long result = (long)TL2.OK;
+            switch (tlm.type)
+            {
+                case TL2.GETSIZE:
+                    if (SrvPos.ContainsKey(msg))
+                        result = SrvPos[msg].Size;
+                    else result = 0;
+                    break;
+                case TL2.AVGPRICE:
+                    if (SrvPos.ContainsKey(msg))
+                        result = WMUtil.pack(SrvPos[msg].AvgPrice);
+                    else result = 0;
+                    break;
+                case TL2.NDAYHIGH:
+                    if (highs.ContainsKey(msg))
+                        result = WMUtil.pack(highs[msg]);
+                    else result = 0;
+                    break;
+                case TL2.NDAYLOW:
+                    if (lows.ContainsKey(msg))
+                        result = WMUtil.pack(lows[msg]);
+                    else result = 0;
+                    break;
+                case TL2.SENDORDER:
+                    SrvDoExecute(msg);
+                    break;
+                case TL2.REGISTERCLIENT:
+                    SrvRegClient(msg);
+                    break;
+                case TL2.REGISTERSTOCK:
+                    string[] m2 = msg.Split('+');
+                    SrvRegStocks(m2[0], m2[1]);
+                    break;
+                case TL2.REGISTERINDEX:
+                    string[] ib = msg.Split('+');
+                    SrvRegIndex(ib[0], ib[1]);
+                    break;
+                case TL2.CLEARCLIENT:
+                    SrvClearClient(msg);
+                    break;
+                case TL2.CLEARSTOCKS:
+                    SrvClearStocks(msg);
+                    break;
+                case TL2.HEARTBEAT:
+                    SrvBeatHeart(msg);
+                    break;
+                default:
+                    result = (long)TL2.UNKNOWNMSG;
+                    break;
+            }
+            m.Result = (IntPtr)result;
+        }
+    }
+}
