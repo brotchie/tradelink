004ed218c5ee3d616559b3cd2c00880ef13ff17d
frantaparallel <frantaparallel@ccc6a032-7b47-0410-9fd8-1f29c74ce1d2>
2008-06-09 15:39:19 +0000

    fixed bug when boxlists were loaded.  moved PrettyError function from tradelink class to here.
    git-svn-id: http://tradelink.googlecode.com/svn/trunk@249 ccc6a032-7b47-0410-9fd8-1f29c74ce1d2

--- TradeLib/Util.cs
+++ TradeLib/Util.cs
@@ -202,7 +202,7 @@ namespace TradeLib
         }
             
 
-        static bool IsBox(Type t) { return (t.BaseType.IsSubclassOf(typeof(Box))) || t.BaseType.Equals(typeof(Box)); }
+        static bool IsBox(Type t) { return (t.BaseType!=null) && ((t.BaseType.IsSubclassOf(typeof(Box))) || t.BaseType.Equals(typeof(Box))); }
         /// <summary>
         /// Gets teh fully qualified boxnames found in a given file.
         /// </summary>
@@ -211,6 +211,7 @@ namespace TradeLib
         public static List<string> GetBoxList(string boxdll)
         {
             List<string> boxlist = new List<string>();
+            if (!File.Exists(boxdll)) return boxlist;
             Assembly a;
             try
             {
@@ -236,6 +237,33 @@ namespace TradeLib
             for (int i = 0; i < t.GetLength(0); i++)
                 if (IsBox(t[i])) boxlist.Add(t[i].FullName);
             return boxlist;
+        }
+
+        /// <summary>
+        /// Gets a user-friendly string for Broker and TradeLink error messages.
+        /// </summary>
+        /// <param name="errorcode">The errorcode.</param>
+        /// <returns></returns>
+        public static string PrettyError(Brokers broker, int errorcode)
+        {
+            // assent errors
+            string[] anvil = { "SO_OK", "SO_NO_ACCOUNT", "SO_NO_SERVER_CONNECTION", "SO_STOCK_NOT_INITIALIZED", "SO_BUYING_POWER_EXCEEDED", "SO_SIZE_ZERO", "SO_INCORRECT_PRICE", "SO_INCORRECT_SIDE", "SO_NO_BULLETS_FOR_CHEAP_STOCK", "SO_NO_SHORTSELL_FOR_CHEAP_STOCK", "SO_NO_ONOPENORDER_FOR_NASDAQ_STOCK", "SO_NO_ONCLOSEORDER_FOR_NASDAQ_STOCK", "SO_NO_ONCLOSEORDER_AGAINST_IMBALANCE_AFTER_1540", "SO_NO_ONCLOSEORDER_AFTER_1600", "SO_NO_SIZEORDER_FOR_NON_NASDAQ_STOCK", "SO_NO_STOPORDER_FOR_NASDAQ_STOCK", "SO_MAX_ORDER_SIZE_EXCEEDED", "SO_MAX_POSITION_SIZE_EXCEEDED", "SO_MAX_POSITION_VALUE_EXCEEDED", "SO_TRADING_LOCKED", "SO_TRADING_HISTORY_NOT_LOADED", "SO_NO_SOES_ORDER_WHEN_MARKET_CLOSED", "SO_NO_SDOT_ORDER_WHEN_MARKET_CLOSED", "SO_MAX_OPEN_POSITIONS_EXCEEDED", "SO_MAX_POSITION_PENDING_ORDERS_EXCEEDED", "SO_MAX_TOTAL_SHARES_EXCEEDED", "SO_MAX_TRADED_SHARES_EXCEEDED", "SO_AMEX_ORDER_EXECUTION_BLOCKED", "SO_NYSE_ORDER_EXECUTION_BLOCKED", "SO_NASDAQ_ORDER_EXECUTION_BLOCKED", "SO_ARCA_ORDER_EXECUTION_BLOCKED", "SO_MAX_LOSS_EXCEEDED", "SO_MAX_LOSS_PER_STOCK_EXCEEDED", "SO_MAX_OPEN_LOSS_PER_STOCK_EXCEEDED", "SO_NYSE_ODD_LOT_VIOLATION", "SO_SHORT_EXEMPT_NOT_INSTITUTIONAL", "SO_SELL_SIZE_GREATER_THAN_POSITION", "SO_NO_SHORT_BEFORE_SELL_COVER_POSITION", "SO_SHORT_CAN_EXECUTE_BEFORE_SELL", "SO_SAME_PRICE_VENUE_OVERSELL", "SO_STAGING_TICKET_EXCEEDED", "SO_DESTINATION_NOT_RECOGNIZED", "SO_HIT_OWN_ORDERS", "SO_SHORT_SELL_VIOLATION", "SO_POTENTIAL_OVERSELL" };
+            TL2 message = (TL2)errorcode;
+            switch (message)
+            {
+                // tradelink messages
+                case TL2.UNKNOWNSYM: return "Unknown symbol.";
+                case TL2.UNKNOWNMSG: return "Unknown message.";
+                case TL2.TL_CONNECTOR_MISSING: return "TradeLink Server not found.";
+                case TL2.GOTNULLORDER: return "Unable to read order.";
+                case TL2.OK: return "Ok";
+                default:
+                    // broker-specific messages
+                    if ((errorcode < anvil.Length) && (broker == Brokers.Assent))
+                        return anvil[errorcode];
+                    break;
+            }
+            return "Unknown error: " + errorcode.ToString();
         }
         
     }
