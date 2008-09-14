f44611528d9b99ff274d734b753c10d8e4c55caa
frantaparallel@gmail.com <frantaparallel@gmail.com@ccc6a032-7b47-0410-9fd8-1f29c74ce1d2>
2008-09-14 23:59:47 +0000

    added duplicate alwaysenter that enters only with single contract/share.
    git-svn-id: http://tradelink.googlecode.com/svn/trunk@467 ccc6a032-7b47-0410-9fd8-1f29c74ce1d2

---
+++ Box-Examples/AlwaysEnterSingle.cs
@@ -0,0 +1,38 @@
+using System;
+using TradeLib;
+
+namespace box
+{
+    /// <summary>
+    /// Enters with only a single share/contract 
+    ///
+    /// This box will always enter long on every tick, unless he's already in a position.
+    /// 
+    /// If he's in a position, he will exit at pre-defined stop and loss targets.  (then get back in)
+    /// 
+    /// Used for testing applications that use boxes.  (Quotopia, Gauntlet, etc)
+    /// </summary>
+    public class AlwaysEnterSingle : BlackBoxEasy
+    {
+        public AlwaysEnterSingle() : base() 
+        {
+            Name = "AlwaysEnter";
+            MinSize = 1;
+        }
+        protected override bool EnterLong()
+        {
+            D("Entering Long");
+            return true;
+        }
+        protected override bool EnterShort()
+        {
+            return false;
+        }
+        protected override bool Exit()
+        {
+            bool exit = (Profit > .15m) || (Profit < -.1m);
+            if (exit) D("Exiting: " + Profit);
+            return exit;
+        }
+    }
+}
