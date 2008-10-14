933c25e5f29905154dfd41cc326e73177e6d2344
frantaparallel@gmail.com <frantaparallel@gmail.com@ccc6a032-7b47-0410-9fd8-1f29c74ce1d2>
2008-10-14 19:44:03 +0000

    added TLPosition class to match c# TradeLib
    git-svn-id: http://tradelink.googlecode.com/svn/trunk@595 ccc6a032-7b47-0410-9fd8-1f29c74ce1d2

---
+++ BrokerServers/TradeLinkServer/TLPosition.cpp
@@ -0,0 +1,13 @@
+#include "stdafx.h"
+#include "TLPosition.h"
+
+namespace TradeLinkServer
+{
+	TLPosition::TLPosition()
+	{
+	}
+
+	TLPosition::~TLPosition()
+	{
+	}
+}
---
+++ BrokerServers/TradeLinkServer/TLPosition.h
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace TradeLinkServer 
+{
+	class AFX_EXT_CLASS TLPosition 
+	{
+	public:
+		TLPosition(void);
+		~TLPosition(void);
+	};
+}
