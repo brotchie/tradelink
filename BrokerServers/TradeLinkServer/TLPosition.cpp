8012d7e16ac7e5a60245fe6a8a64f86fe3ed5979
frantaparallel@gmail.com <frantaparallel@gmail.com@ccc6a032-7b47-0410-9fd8-1f29c74ce1d2>
2008-10-14 20:53:20 +0000

    added serialization/deserialization and closed pl features
    git-svn-id: http://tradelink.googlecode.com/svn/trunk@596 ccc6a032-7b47-0410-9fd8-1f29c74ce1d2

--- BrokerServers/TradeLinkServer/TLPosition.cpp
+++ BrokerServers/TradeLinkServer/TLPosition.cpp
@@ -1,10 +1,51 @@
 #include "stdafx.h"
 #include "TLPosition.h"
+#include "Util.h"
 
 namespace TradeLinkServer
 {
-	TLPosition::TLPosition()
+
+	TLPosition::TLPosition() 
+	{ 
+		Symbol = "";
+		AvgPrice = 0;
+		Size = 0;
+		ClosedPL = 0;
+	}
+
+	TLPosition::TLPosition(CString symbol, double avgprice, int possize)
 	{
+		Symbol = symbol;
+		AvgPrice = avgprice;
+		Size = possize;
+		ClosedPL = 0;
+	}
+
+	TLPosition::TLPosition(CString symbol)
+	{
+		Symbol = symbol;
+		AvgPrice = 0;
+		Size = 0;
+		ClosedPL = 0;
+	}
+
+	CString TLPosition::Serialize()
+	{
+		CString m;
+		m.Format("%s,%f,%i,%f",Symbol,AvgPrice,Size,ClosedPL);
+		return m;
+	}
+
+	TLPosition TLPosition::Deserialize(CString msg)
+	{
+		TLPosition pos;
+		std::vector<CString> r;
+		gsplit(msg,CString(","),r);
+		pos.Symbol = r[psym];
+		pos.AvgPrice = _tstof(r[pavg]);
+		pos.Size = _tstoi(r[psiz]);
+		pos.ClosedPL = _tstof(r[pcpl]);
+		return pos;
 	}
 
 	TLPosition::~TLPosition()
--- BrokerServers/TradeLinkServer/TLPosition.h
+++ BrokerServers/TradeLinkServer/TLPosition.h
@@ -7,5 +7,21 @@ namespace TradeLinkServer
 	public:
 		TLPosition(void);
 		~TLPosition(void);
+		TLPosition(CString sym);
+		TLPosition(CString sym, double price, int size);
+		CString Symbol;
+		double ClosedPL;
+		double AvgPrice;
+		int Size;
+		CString Serialize();
+		static TLPosition Deserialize(CString msg);
+	};
+
+	enum PosField
+	{
+		psym,
+		pavg,
+		psiz,
+		pcpl,
 	};
 }
