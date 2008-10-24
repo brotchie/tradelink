8bbd89fed321ba28d90727e18c86385136a76b45
frantaparallel@gmail.com <frantaparallel@gmail.com@ccc6a032-7b47-0410-9fd8-1f29c74ce1d2>
2008-10-24 21:20:41 +0000

    issue #44 work begun
    git-svn-id: http://tradelink.googlecode.com/svn/trunk@635 ccc6a032-7b47-0410-9fd8-1f29c74ce1d2

---
+++ SterServer/SterMain.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace SterServer
+{
+    partial class SterMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SterMain));
+            this.msgbox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // msgbox
+            // 
+            this.msgbox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.msgbox.FormattingEnabled = true;
+            this.msgbox.ItemHeight = 20;
+            this.msgbox.Location = new System.Drawing.Point(0, 0);
+            this.msgbox.Name = "msgbox";
+            this.msgbox.Size = new System.Drawing.Size(278, 244);
+            this.msgbox.TabIndex = 0;
+            // 
+            // SterMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(278, 248);
+            this.Controls.Add(this.msgbox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "SterMain";
+            this.Text = "SterServer";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox msgbox;
+    }
+}
+
---
+++ SterServer/SterMain.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Forms;
+using TradeLib;
+using SterlingLib;
+
+namespace SterServer
+{
+    public partial class SterMain : Form
+    {
+        // basic structures needed for operation
+        STIEvents stiEvents = new STIEvents();
+        STIOrderMaint stiOrder = new STIOrderMaint();
+        STIPosition stiPos = new STIPosition();
+        STIQuote stiQuote = new STIQuote();
+        TradeLink_Server_WM tl = new TradeLink_Server_WM(TLTypes.LIVEBROKER);
+        const string PROGRAM = "SterServer ";
+
+        public SterMain()
+        {
+            InitializeComponent();
+            stiEvents.OnSTIOrderConfirm += new _ISTIEventsEvents_OnSTIOrderConfirmEventHandler(stiEvents_OnSTIOrderConfirm);
+            stiEvents.OnSTITradeUpdate += new _ISTIEventsEvents_OnSTITradeUpdateEventHandler(stiEvents_OnSTITradeUpdate);
+            stiPos.OnSTIPositionUpdate += new _ISTIPositionEvents_OnSTIPositionUpdateEventHandler(stiPos_OnSTIPositionUpdate);
+            stiQuote.OnSTIQuoteUpdate += new _ISTIQuoteEvents_OnSTIQuoteUpdateEventHandler(stiQuote_OnSTIQuoteUpdate);
+            tl.gotSrvFillRequest += new OrderDelegate(tl_gotSrvFillRequest);
+            tl.gotSrvPosList += new TradeLink_Server_WM.PositionArrayDelegate(tl_gotSrvPosList);
+            
+            debug(PROGRAM + Util.TLSIdentity());
+        }
+
+        void stiEvents_OnSTITradeUpdate(ref structSTITradeUpdate t)
+        {
+            Trade f = new Trade();
+            f.Account = t.bstrAccount;
+            f.id = Convert.ToUInt16(t.bstrClOrderId);
+            f.Currency = (Currency)Enum.Parse(typeof(Currency), t.bstrCurrency);
+            f.xprice = (decimal)t.fExecPrice;
+            f.xsize = t.nQuantity;
+            DateTime now = DateTime.Parse(t.bstrUpdateTime);
+            f.xdate = Util.ToTLDate(now);
+            f.xtime = Util.ToTLTime(now);
+            f.xsec = now.Second;
+            tl.newFill(f);
+        }
+
+        Position[] tl_gotSrvPosList(string account)
+        {
+            stiPos.RegisterForPositions();
+            return new Position[0];
+        }
+
+        void stiQuote_OnSTIQuoteUpdate(ref structSTIQuoteUpdate q)
+        {
+            Tick k = new Tick(q.bstrSymbol);
+            k.bid = (decimal)q.fBidPrice;
+            k.ask = (decimal)q.fAskPrice;
+            k.bs = q.nBidSize;
+            k.os = q.nAskSize;
+            k.be = q.bstrBidExch;
+            k.oe = q.bstrAskExch;
+            DateTime now = DateTime.Parse(q.bstrUpdateTime);
+            k.date = Util.ToTLDate(now);
+            k.time = Util.ToTLTime(now);
+            k.sec = now.Second;
+            k.trade = (decimal)q.fLastPrice;
+            k.size = q.nLastSize;
+            tl.newTick(k);
+        }
+
+        void tl_gotSrvFillRequest(Order o)
+        {
+            STIOrder order = new STIOrder();
+            order.Symbol = o.symbol;
+            order.Quantity = o.UnSignedSize;
+            order.Account = o.Account;
+            order.Destination = o.Exchange;
+            order.Tif = o.TIF;
+            order.PriceType = o.isMarket ? STIPriceTypes.ptSTIMkt : (o.isLimit ? STIPriceTypes.ptSTILmt : STIPriceTypes.ptSTISvrStp);
+            order.ClOrderID = o.id.ToString();
+            int err = order.SubmitOrder();
+            if (err < 0)
+                debug("Error sending order: " + Util.PrettyError(Brokers.Echo, err) + o.ToString());
+
+        }
+
+        void stiPos_OnSTIPositionUpdate(ref structSTIPositionUpdate structPositionUpdate)
+        {
+            debug("got position");
+        }
+
+        void stiEvents_OnSTIOrderConfirm(ref structSTIOrderConfirm structOrderConfirm)
+        {
+            debug("got order confirm");
+        }
+
+        void debug(string msg)
+        {
+            if (msgbox.InvokeRequired)
+                msgbox.Invoke(new DebugDelegate(debug), new object[] { msg });
+            else
+            {
+                msgbox.Items.Add(msg);
+                msgbox.SelectedIndex = msgbox.Items.Count - 1;
+            }
+        }
+    }
+}
