6c100b8b86a1ad0fbfefe981dca57a352fb71cb8
frantaparallel@gmail.com <frantaparallel@gmail.com@ccc6a032-7b47-0410-9fd8-1f29c74ce1d2>
2008-11-07 20:29:48 +0000

    added stub class for tradelink client (issue #33)
    git-svn-id: http://tradelink.googlecode.com/svn/trunk@674 ccc6a032-7b47-0410-9fd8-1f29c74ce1d2

---
+++ BrokerServers/TradeLibFast/TLClient_WM.cpp
@@ -0,0 +1,19 @@
+#include "stdafx.h"
+#include "TLClient_WM.h"
+
+
+namespace TradeLibFast
+{
+	// TLClient_WM
+
+	IMPLEMENT_DYNAMIC(TLClient_WM, CWnd)
+
+	TLClient_WM::TLClient_WM()
+	{
+	}
+
+	TLClient_WM::~TLClient_WM()
+	{
+	}
+
+}
---
+++ BrokerServers/TradeLibFast/TLClient_WM.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "TradeLibFast.h"
+
+
+namespace TradeLibFast
+{
+	class AFX_EXT_CLASS  TLClient_WM : public CWnd
+	{
+		DECLARE_DYNAMIC(TLClient_WM)
+
+	public:
+		TLClient_WM(void);
+		~TLClient_WM(void);
+	};
+}
