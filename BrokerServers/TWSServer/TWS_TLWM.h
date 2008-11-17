6bce344df6ee4ce8ee20049c1bb455da14262267
frantaparallel@gmail.com <frantaparallel@gmail.com@ccc6a032-7b47-0410-9fd8-1f29c74ce1d2>
2008-11-17 02:37:48 +0000

    test for pipeserver
    git-svn-id: http://tradelink.googlecode.com/svn/trunk@690 ccc6a032-7b47-0410-9fd8-1f29c74ce1d2

--- BrokerServers/TWSServer/TWS_TLWM.h
+++ BrokerServers/TWSServer/TWS_TLWM.h
@@ -7,13 +7,15 @@
 #include "orderstate.h"
 #include "Contract.h"
 #include "TLServer_WM.h"
+#include "TLServer_NP.h"
 
 namespace TradeLibFast
 {
 
 	class TWS_TLWM :
 		public TLServer_WM ,
-		public EWrapper
+		public EWrapper, 
+		public TLServer_NP
 	{
 	public:
 		TWS_TLWM(void);
