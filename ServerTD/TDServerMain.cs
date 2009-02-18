5f85fdad43a5066f3b48f4af56596a751dd3f7bf
frantaparallel@gmail.com <frantaparallel@gmail.com@ccc6a032-7b47-0410-9fd8-1f29c74ce1d2>
2009-02-18 21:07:00 +0000

    issue #147 begun (skeleton dll project imported, just need to bind events and test)
    git-svn-id: http://tradelink.googlecode.com/svn/trunk@982 ccc6a032-7b47-0410-9fd8-1f29c74ce1d2

---
+++ ServerTD/TDServerMain.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using TradeLink.API;
+using TradeLink.Common;
+using AMTD_API;
+
+namespace TDServer
+{
+    public partial class TDServerMain : Form
+    {
+        AmeritradeBrokerAPI api = new AmeritradeBrokerAPI();
+        TLServer_WM tl = new TLServer_WM(TLTypes.LIVEBROKER);
+        public TDServerMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
