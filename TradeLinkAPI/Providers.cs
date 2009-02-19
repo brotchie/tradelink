b6482df5c23404866656bfa027e68dfc7a52a2d2
frantaparallel@gmail.com <frantaparallel@gmail.com@ccc6a032-7b47-0410-9fd8-1f29c74ce1d2>
2009-02-19 18:50:33 +0000

    renamed forgot to include from issue #144
    git-svn-id: http://tradelink.googlecode.com/svn/trunk@986 ccc6a032-7b47-0410-9fd8-1f29c74ce1d2

---
+++ TradeLinkAPI/Providers.cs
@@ -0,0 +1,21 @@
+﻿
+namespace TradeLink.API
+{
+    public enum Providers
+    {
+        Unknown = -1,
+        TradeLinkSimulation = 0,
+        Assent,
+        InteractiveBrokers,
+        Genesis,
+        Bright,
+        Echo,
+        Sterling,
+        TDAmeritrade,
+        Blackwood,
+        MBTrading,
+        HUBB,
+        Tradespeed,
+        REDI,
+    }
+}
