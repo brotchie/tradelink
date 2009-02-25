2f53d27f378404148f7b0093dfb9082af39724be
frantaparallel@gmail.com <frantaparallel@gmail.com@ccc6a032-7b47-0410-9fd8-1f29c74ce1d2>
2009-02-25 20:52:35 +0000

    issue #158 completed.
    git-svn-id: http://tradelink.googlecode.com/svn/trunk@999 ccc6a032-7b47-0410-9fd8-1f29c74ce1d2

---
+++ TradeLinkCommon/PositionTracker.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Text;
+using TradeLink.API;
+
+namespace TradeLink.Common
+{
+
+    public class PositionTracker
+    {
+        public PositionTracker() { }
+        Dictionary<string, Position> posdict = new Dictionary<string, Position>();
+
+        public Position this[string symbol] { get { Position p; if (posdict.TryGetValue(symbol, out p)) return p; return new PositionImpl(symbol);  } }
+        public IEnumerator GetEnumerator() { foreach (Position p in posdict.Values) yield return p; }
+        
+        /// <summary>
+        /// Create a new position, or overwrite existing position
+        /// </summary>
+        /// <param name="newpos"></param>
+        public void NewPosition(Position newpos)
+        {
+            Position p;
+            if (posdict.TryGetValue(newpos.Symbol, out p))
+                posdict[newpos.Symbol] = newpos;
+            else
+                posdict.Add(newpos.Symbol, newpos);
+        }
+
+        /// <summary>
+        /// Adjust an existing position, or create new one if none exists.
+        /// </summary>
+        /// <param name="fill"></param>
+        /// <returns>any closed PL for adjustment</returns>
+        public decimal Adjust(Trade fill)
+        {
+            Position p;
+            decimal cpl = 0;
+            if (posdict.TryGetValue(fill.symbol, out p))
+                cpl += posdict[fill.symbol].Adjust(fill);
+            else
+                posdict.Add(fill.symbol, new PositionImpl(fill));
+            return cpl;
+        }
+
+
+
+    }
+}
---
+++ TradeLinkTests/TestPositionTracker.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using TradeLink.API;
+using TradeLink.Common;
+
+namespace TestTradeLink
+{
+    [TestFixture]
+    public class TestPositionTracker
+    {
+        public TestPositionTracker() { }
+
+        [Test]
+        public void Adjust()
+        {
+            const string s = "IBM";
+            TradeImpl t1 = new TradeImpl(s, 100, 100);
+
+            PositionTracker pt = new PositionTracker();
+
+            // make we have no position yet
+            Assert.IsTrue(pt[t1.symbol].isFlat);
+
+            // send some adjustments
+            decimal cpl = 0;
+            cpl += pt.Adjust(t1);
+            cpl += pt.Adjust(t1);
+
+            // verify that adjustments took hold
+            Assert.AreEqual(0, cpl);
+            Assert.AreEqual(200, pt[t1.symbol].Size);
+        }
+
+        [Test]
+        public void NewPosition()
+        {
+            const string s = "IBM";
+            Position p = new PositionImpl(s,80,500);
+
+            PositionTracker pt = new PositionTracker();
+            Assert.IsTrue(pt[s].isFlat);
+            pt.NewPosition(p);
+            Assert.AreEqual(500, pt[s].Size);
+
+
+
+        }
+    }
+}
