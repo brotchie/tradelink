8904f6f8d29fe73ab4394d0b7eeb97faf6dc7bde
frantaparallel@gmail.com <frantaparallel@gmail.com@ccc6a032-7b47-0410-9fd8-1f29c74ce1d2>
2009-03-18 00:53:07 +0000

    issue #177 completed.
    git-svn-id: http://tradelink.googlecode.com/svn/trunk@1079 ccc6a032-7b47-0410-9fd8-1f29c74ce1d2

--- TradeLinkCommon/Calc.cs
+++ TradeLinkCommon/Calc.cs
@@ -4,7 +4,7 @@ using TradeLink.API;
 
 namespace TradeLink.Common
 {
-    public class Calc
+    public static class Calc
     {
         /// <summary>
         /// Gets the open PL on a per-share basis, ignoring the size of the position.
@@ -165,6 +165,485 @@ namespace TradeLink.Common
             int size = !normalizesize ? (int)(p.FlatSize * percent) : Norm2Min(p.FlatSize * percent, MINSIZE);
             o = new StopOrder(p.Symbol, !p.isLong, size, price);
             return o;
+        }
+        /// <summary>
+        /// sum last elements of array
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="barsback"></param>
+        /// <returns></returns>
+        public static decimal Sum(decimal[] array, int barsback) { return Sum(array, array.Length - barsback, barsback); }
+        /// <summary>
+        /// sum part of an array
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="startindex"></param>
+        /// <param name="length"></param>
+        /// <returns></returns>
+        public static decimal Sum(decimal[] array, int startindex, int length)
+        {
+            decimal sum = 0;
+            for (int i = startindex; i < startindex + length; i++)
+                sum += array[i];
+            return sum;
+        }
+        /// <summary>
+        /// gets sum of entire array
+        /// </summary>
+        /// <param name="array"></param>
+        /// <returns></returns>
+        public static decimal Sum(decimal[] array) { return Sum(array, 0, array.Length); }
+        /// <summary>
+        /// gets sum of squares for end of an array
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="barsback"></param>
+        /// <returns></returns>
+        public static decimal SumSquares(decimal[] array, int barsback) { return SumSquares(array, array.Length - barsback, barsback); }
+        /// <summary>
+        /// get sums of squares for part of an array
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="startindex"></param>
+        /// <param name="length"></param>
+        /// <returns></returns>
+        public static decimal SumSquares(decimal[] array, int startindex, int length)
+        {
+            decimal sum = 0;
+            for (int i = startindex; i < startindex + length; i++)
+                sum += array[i]*array[i];
+            return sum;
+        }
+
+        /// <summary>
+        /// gets sum of squares for entire array
+        /// </summary>
+        /// <param name="array"></param>
+        /// <returns></returns>
+        public static decimal SumSquares(decimal[] array) { return SumSquares(array, 0, array.Length); }
+
+        /// <summary>
+        /// sum last elements of array
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="barsback"></param>
+        /// <returns></returns>
+        public static int Sum(int[] array, int barsback) { return Sum(array, array.Length - barsback, barsback); }
+        /// <summary>
+        /// sum part of an array
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="startindex"></param>
+        /// <param name="length"></param>
+        /// <returns></returns>
+        public static int Sum(int[] array, int startindex, int length)
+        {
+            int sum = 0;
+            for (int i = startindex; i < startindex + length; i++)
+                sum += array[i];
+            return sum;
+        }
+        /// <summary>
+        /// gets sum of entire array
+        /// </summary>
+        /// <param name="array"></param>
+        /// <returns></returns>
+        public static int Sum(int[] array) { return Sum(array, 0, array.Length); }
+        /// <summary>
+        /// gets sum of squares for end of an array
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="barsback"></param>
+        /// <returns></returns>
+        public static int SumSquares(int[] array, int barsback) { return SumSquares(array, array.Length - barsback, barsback); }
+        /// <summary>
+        /// get sums of squares for part of an array
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="startindex"></param>
+        /// <param name="length"></param>
+        /// <returns></returns>
+        public static int SumSquares(int[] array, int startindex, int length)
+        {
+            int sum = 0;
+            for (int i = startindex; i < startindex + length; i++)
+                sum += array[i] * array[i];
+            return sum;
+
+        }
+
+        /// <summary>
+        /// gets sum of squares for entire array
+        /// </summary>
+        /// <param name="array"></param>
+        /// <returns></returns>
+        public static int SumSquares(int[] array) { return SumSquares(array, 0, array.Length); }
+
+        /// <summary>
+        /// gets mean of an array
+        /// </summary>
+        /// <param name="array"></param>
+        /// <returns></returns>
+        public static decimal Avg(decimal[] array) { return Avg(array, true); }
+        /// <summary>
+        /// gets mean of an array
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="returnzeroIfempty"></param>
+        /// <returns></returns>
+        public static decimal Avg(decimal[] array, bool returnzeroIfempty) { return array.Length == 0 ? 0 : Sum(array) / array.Length; }
+        public static decimal Avg(int[] array ) { return Avg(array,true); }
+        public static decimal Avg(int[] array, bool returnzeroIfempty) { return array.Length == 0 ? 0 : (decimal)Sum(array) / array.Length; }
+
+        /// <summary>
+        /// adds two arrays
+        /// </summary>
+        /// <param name="array1"></param>
+        /// <param name="array2"></param>
+        /// <returns></returns>
+        public static int[] Add(int[] array1, int[] array2)
+        {
+            // normalize sizes of arrays
+            bool a2bigger = array1.Length < array2.Length ;
+            int max = a2bigger ? array2.Length : array1.Length;
+            int[] s1 = new int[max];
+            int[] s2 = new int[max];
+            Buffer.BlockCopy(array1,0,s1,0,array1.Length*4);
+            Buffer.BlockCopy(array2,0,s2,0,array2.Length*4);
+            // calculate values
+            for (int i = 0; i < s1.Length; i++)
+                s2[i] += s1[i];
+            return s2;
+        }
+
+        /// <summary>
+        /// adds two arrays
+        /// </summary>
+        /// <param name="array1"></param>
+        /// <param name="array2"></param>
+        /// <returns></returns>
+        public static decimal[] Add(decimal[] array1, decimal[] array2)
+        {
+            // normalize sizes of arrays
+            bool a2bigger = array1.Length < array2.Length;
+            int max = a2bigger ? array2.Length : array1.Length;
+            long[] s1 = decimal2long(array1, max);
+            long[] s2 = decimal2long(array2, max);
+            // calculate values
+            for (int i = 0; i < s1.Length; i++)
+                s2[i] += s1[i];
+            return long2decimal(s2);
+        }
+
+        const uint D2L_MULT = 65536;
+        static long[] decimal2long(decimal[] a) { return decimal2long(a, a.Length); }
+        static long[] decimal2long(decimal[] a, int length)
+        {
+            long[] r = new long[length];
+            for (int i = 0; i < length; i++)
+                r[i] = (long)(a[i] * D2L_MULT);
+            return r;
+        }
+
+        static decimal[] long2decimal(long[] a) { return long2decimal(a, a.Length); }
+        static decimal[] long2decimal(long[] a, int length)
+        {
+            decimal[] r = new decimal[length];
+            for (int i = 0; i < length; i++)
+                r[i] = ((decimal)a[i] / D2L_MULT);
+            return r;
+        }
+
+        static decimal[] long2decimalp(long[] a)
+        {
+            decimal[] r = new decimal[a.Length];
+            long m = D2L_MULT*(long)D2L_MULT;
+            for (int i = 0; i < a.Length; i++)
+                r[i] = ((decimal)a[i] / m);
+            return r;
+        }
+
+        /// <summary>
+        /// subtracts 2nd array from first array
+        /// </summary>
+        /// <param name="array1"></param>
+        /// <param name="array2"></param>
+        /// <returns></returns>
+        public static int[] Subtract(int[] array1, int[] array2)
+        {
+            // normalize sizes of arrays
+            bool a2bigger = array1.Length < array2.Length;
+            int max = a2bigger ? array2.Length : array1.Length;
+            int[] s1 = new int[max];
+            int[] s2 = new int[max];
+            Buffer.BlockCopy(array1, 0, s1, 0, array1.Length * 4);
+            Buffer.BlockCopy(array2, 0, s2, 0, array2.Length * 4);
+            // calculate values
+            for (int i = 0; i < s1.Length; i++)
+                s1[i] -= s2[i];
+            return s1;
+        }
+
+        /// <summary>
+        /// subtracts 2nd array from first array
+        /// </summary>
+        /// <param name="array1"></param>
+        /// <param name="array2"></param>
+        /// <returns></returns>
+        public static decimal[] Subtract(decimal[] array1, decimal[] array2)
+        {
+            // normalize sizes of arrays
+            bool a2bigger = array1.Length < array2.Length;
+            int max = a2bigger ? array2.Length : array1.Length;
+            long[] s1 = decimal2long(array1, max);
+            long[] s2 = decimal2long(array2, max);
+            // calculate values
+            for (int i = 0; i < s1.Length; i++)
+                s1[i] -= s2[i];
+            return long2decimal(s1);
+        }
+
+        /// <summary>
+        /// multiplies two arrays
+        /// </summary>
+        /// <param name="array1"></param>
+        /// <param name="array2"></param>
+        /// <returns></returns>
+        public static int[] Product(int[] array1, int[] array2)
+        {
+            // normalize sizes of arrays
+            bool a2bigger = array1.Length < array2.Length;
+            int max = a2bigger ? array2.Length : array1.Length;
+            int[] s1 = new int[max];
+            int[] s2 = new int[max];
+            Buffer.BlockCopy(array1, 0, s1, 0, array1.Length * 4);
+            Buffer.BlockCopy(array2, 0, s2, 0, array2.Length * 4);
+            // calculate values
+            for (int i = 0; i < s1.Length; i++)
+                s2[i] *= s1[i];
+            return s2;
+        }
+
+        /// <summary>
+        /// multiplies two arrays
+        /// </summary>
+        /// <param name="array1"></param>
+        /// <param name="array2"></param>
+        /// <returns></returns>
+        public static decimal[] Product(decimal[] array1, decimal[] array2)
+        {
+            // normalize sizes of arrays
+            bool a2bigger = array1.Length < array2.Length;
+            int max = a2bigger ? array2.Length : array1.Length;
+            long[] s1 = decimal2long(array1, max);
+            long[] s2 = decimal2long(array2, max);
+            // calculate values
+            for (int i = 0; i < s1.Length; i++)
+                s2[i] *= s1[i];
+            return long2decimalp(s2);
+        }
+
+        /// <summary>
+        /// divides first array by second array.  
+        /// </summary>
+        /// <param name="array1"></param>
+        /// <param name="array2"></param>
+        /// <returns></returns>
+        public static decimal[] Divide(int[] array1, int[] array2) { return Divide(array1,array2, true); }
+        public static decimal[] Divide(int[] array1, int[] array2,bool zeroIfundefined)
+        {
+            // normalize sizes of arrays
+            bool a2bigger = array1.Length < array2.Length;
+            int max = a2bigger ? array2.Length : array1.Length;
+            int[] s1 = new int[max];
+            int[] s2 = new int[max];
+            decimal[] r = new decimal[max];
+            Buffer.BlockCopy(array1, 0, s1, 0, array1.Length * 4);
+            Buffer.BlockCopy(array2, 0, s2, 0, array2.Length * 4);
+            // calculate values
+            for (int i = 0; i < s1.Length; i++)
+                if (zeroIfundefined)
+                    r[i] = s2[i] != 0 ? (decimal)s1[i] / s2[i] : 0;
+                else 
+                    r[i] = (decimal)s1[i] / s2[i];
+            return r;
+        }
+
+        /// <summary>
+        /// divides first array by second array.  
+        /// </summary>
+        /// <param name="array1"></param>
+        /// <param name="array2"></param>
+        /// <returns></returns>
+        public static decimal[] Divide(decimal[] array1, decimal[] array2) { return Divide(array1, array2, true); }
+        public static decimal[] Divide(decimal[] array1, decimal[] array2, bool zeroIfundefined)
+        {
+            // normalize sizes of arrays
+            bool a2bigger = array1.Length < array2.Length;
+            int max = a2bigger ? array2.Length : array1.Length;
+            long[] s1 = decimal2long(array1, max);
+            long[] s2 = decimal2long(array2, max);
+            decimal[] r = new decimal[max];
+            // calculate values
+            for (int i = 0; i < s1.Length; i++)
+            {
+                if (zeroIfundefined)
+                    r[i] = s2[i] != 0 ? (decimal)s1[i] / s2[i] : 0;
+                else
+                    r[i] = (decimal)s1[i] / s2[i];
+            }
+            return r;
+        }
+        /// <summary>
+        /// adds a constant to an array
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="val"></param>
+        /// <returns></returns>
+        public static decimal[] Add(int[] array, decimal val)
+        {
+            decimal[] r = new decimal[array.Length];
+            for (int i = 0; i < array.Length; i++)
+                r[i] = array[i] + val;
+            return r;
+        }
+        public static int[] Add(int[] array, int val)
+        {
+            int[] r = new int[array.Length];
+            for (int i = 0; i < array.Length; i++)
+                r[i] = array[i] + val;
+            return r;
+        }
+        public static decimal[] Add(decimal[] array, decimal val)
+        {
+            decimal[] r = new decimal[array.Length];
+            for (int i = 0; i < array.Length; i++)
+                r[i] = array[i] + val;
+            return r;
+        }
+
+        /// <summary>
+        /// subtracts constant from an array
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="val"></param>
+        /// <returns></returns>
+        public static decimal[] Subtract(int[] array, decimal val)
+        {
+            decimal[] r = new decimal[array.Length];
+            for (int i = 0; i < array.Length; i++)
+                r[i] = array[i] - val;
+            return r;
+        }
+        public static int[] Subtract(int[] array, int val)
+        {
+            int[] r = new int[array.Length];
+            for (int i = 0; i < array.Length; i++)
+                r[i] = array[i] - val;
+            return r;
+        }
+        public static decimal[] Subtract(decimal[] array, decimal val)
+        {
+            decimal[] r = new decimal[array.Length];
+            for (int i = 0; i < array.Length; i++)
+                r[i] = array[i] - val;
+            return r;
+        }
+
+
+        /// <summary>
+        /// takes product of constant and an array
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="val"></param>
+        /// <returns></returns>
+        public static decimal[] Product(int[] array, decimal val)
+        {
+            decimal[] r = new decimal[array.Length];
+            for (int i = 0; i < array.Length; i++)
+                r[i] = array[i]*val;
+            return r;
+        }
+        public static int[] Product(int[] array, int val)
+        {
+            int[] r = new int[array.Length];
+            for (int i = 0; i < array.Length; i++)
+                r[i] = array[i] * val;
+            return r;
+        }
+        public static decimal[] Product(decimal[] array, decimal val)
+        {
+            decimal[] r = new decimal[array.Length];
+            for (int i = 0; i < array.Length; i++)
+                r[i] = array[i] * val;
+            return r;
+        }
+        /// <summary>
+        /// divides array by a constant
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="val"></param>
+        /// <returns></returns>
+        public static decimal[] Divide(int[] array, decimal val)
+        {
+            decimal[] r = new decimal[array.Length];
+            for (int i = 0; i < array.Length; i++)
+                r[i] = (decimal)array[i] / val;
+            return r;
+        }
+        public static decimal[] Divide(decimal[] array, decimal val)
+        {
+            decimal[] r = new decimal[array.Length];
+            for (int i = 0; i < array.Length; i++)
+                r[i] = (decimal)array[i] / val;
+            return r;
+        }
+        /// <summary>
+        /// gets standard deviation for values of a population
+        /// </summary>
+        /// <param name="array"></param>
+        /// <returns></returns>
+        public static decimal StdDev(int[] array)
+        {
+            decimal avg = Avg(array);
+            decimal sq = SumSquares(array);
+            decimal tmp = (sq/ array.Length) - (avg * avg);
+            decimal stdev = (decimal)Math.Pow((double)tmp, .5);
+            return stdev;
+        }
+
+        public static decimal StdDev(decimal[] array)
+        {
+            decimal avg = Avg(array);
+            decimal sq = SumSquares(array);
+            decimal tmp = (sq / array.Length) - (avg * avg);
+            decimal stdev = (decimal)Math.Pow((double)tmp, .5);
+            return stdev;
+
+        }
+        /// <summary>
+        /// gets standard deviation for values of a sample
+        /// </summary>
+        /// <param name="array"></param>
+        /// <returns></returns>
+        public static decimal StdDevSam(int[] array)
+        {
+            decimal avg = Avg(array);
+            decimal[] var = Subtract(array, avg);
+            decimal[] varsq = Product(var, var);
+            decimal sumvar = Sum(varsq);
+            decimal stdev = (decimal)Math.Pow((double)sumvar / (array.Length - 1), .5);
+            return stdev;
+        }
+
+        public static decimal StdDevSam(decimal[] array)
+        {
+            decimal avg = Avg(array);
+            decimal[] var = Subtract(array, avg);
+            decimal[] varsq = Product(var, var);
+            decimal sumvar = Sum(varsq);
+            decimal stdev = (decimal)Math.Pow((double)sumvar / (array.Length-1),.5);
+            return stdev;
         }
 
     }
--- TradeLinkTests/TestCalc.cs
+++ TradeLinkTests/TestCalc.cs
@@ -48,5 +48,170 @@ namespace TestTradeLink
             Assert.AreEqual(pl*(lsize/2),Calc.ClosePL(lp, lc)); // matches closing size
             Assert.AreEqual((entry-last)*-ssize, Calc.ClosePL(sp, sc));
         }
+
+        [Test]
+        public void Sums()
+        {
+            // integer
+            int[] a1 = new int[] { 6, 10, 33, 2, -50, 7, 8 };
+            Assert.AreEqual(15,Calc.Sum(a1, 2));
+            Assert.AreEqual(16, Calc.Sum(a1));
+            Assert.AreEqual(45, Calc.Sum(a1, 1, 3));
+
+            // decimal
+            decimal[] a2 = new decimal[] { 6.5m, 10.3m, 33.1m, 2, -50, 7.7m, 8 };
+            Assert.AreEqual(15.7m, Calc.Sum(a2, 2));
+            Assert.AreEqual(17.6, Calc.Sum(a2));
+            Assert.AreEqual(45.4, Calc.Sum(a2, 1, 3));
+
+
+
+        }
+
+        public void SumSquares()
+        {
+            // integer
+            int[] a1 = new int[] { 6, 10, 33, 2, -50, 7, 8 };
+            Assert.AreEqual(113, Calc.SumSquares(a1, 2));
+            Assert.AreEqual(3842, Calc.SumSquares(a1));
+            Assert.AreEqual(1193, Calc.SumSquares(a1, 1, 3));
+
+            // decimal
+            decimal[] a2 = new decimal[] { 6.5m, 10.3m, 33.1m, 2, -50, 7.7m, 8 };
+            Assert.AreEqual(123.29m, Calc.SumSquares(a2, 2));
+            Assert.AreEqual(3871.24m, Calc.SumSquares(a2));
+            Assert.AreEqual(1205.7m, Calc.SumSquares(a2, 1, 3));
+        }
+
+        [Test]
+        public void AvgAndStd()
+        {
+            // data
+            int[] a1 = new int[] { 6, 10, 33, 2, -50, 7, 8 };
+            decimal[] b1 = new decimal[] { 6.4m, 10.1m, 33.33m, 2.7m, -50, 7.1m, 8 };
+
+            // averages
+            Assert.AreEqual(2.2857142857142857142857142857m, Calc.Avg(a1));
+            Assert.AreEqual(2.5185714285714285714285714286m, Calc.Avg(b1));
+
+            // stdev
+            Assert.AreEqual(23.315931314473m, Calc.StdDev(a1));
+            Assert.AreEqual(23.3946162479267m, Calc.StdDev(b1));
+
+            // stdevsample
+            Assert.AreEqual(25.1840788564133m, Calc.StdDevSam(a1));
+            Assert.AreEqual(25.2690694320904m, Calc.StdDevSam(b1));
+
+        }
+
+        [Test]
+        public void ArrayArrayMath()
+        {
+            // integer
+            int[] a1 = new int[] { 6, 10, 33, 2, -50, 7, 8 };
+            int[] a2 = new int[] { 4,0,-3,-2,0,3,2};
+
+            Assert.AreEqual(20, Calc.Sum(Calc.Add(a1, a2)));
+            Assert.AreEqual(12, Calc.Sum(Calc.Subtract(a1, a2)));
+            Assert.AreEqual(-42, Calc.Sum(Calc.Product(a1,a2)));
+            Assert.AreEqual(-4.16666666666666666666666,Calc.Sum(Calc.Divide(a1,a2)));
+
+            // decimal
+            decimal [] b1 = new decimal[] { 6.4m, 10.1m, 33.33m, 2.7m, -50, 7.1m, 8 };
+            decimal [] b2 = new decimal[] { 4.1m,0,-3,-2,0,3,2.7m};
+
+            Assert.AreEqual(2243, Math.Round(Calc.Sum(Calc.Add(b1, b2))*100));
+            Assert.AreEqual(1283, Math.Round(Calc.Sum(Calc.Subtract(b1, b2))*100));
+            Assert.AreEqual(-3625, Math.Round(Calc.Sum(Calc.Product(b1, b2)) * 100));
+            Assert.AreEqual(-557,Math.Round(Calc.Sum(Calc.Divide(b1,b2))*100));
+
+        }
+
+        [Test]
+        public void ArrayScalarMath()
+        {
+            int[] a1 = new int[] { 6, 10, 33, 2, -50, 7, 8 };
+
+            Assert.AreEqual(51, Calc.Sum(Calc.Add(a1, 5)));
+            Assert.AreEqual(-19, Calc.Sum(Calc.Subtract(a1, 5)));
+            Assert.AreEqual(80, Calc.Sum(Calc.Product(a1, 5)));
+            Assert.AreEqual(3.2m, Calc.Sum(Calc.Divide(a1, 5)));
+
+            decimal[] b1 = new decimal[] { 6.4m, 10.1m, 33.33m, 2.7m, -50, 7.1m, 8 };
+            Assert.AreEqual(56.13m, Calc.Sum(Calc.Add(b1, 5.5m)));
+            Assert.AreEqual(-20.87m, Calc.Sum(Calc.Subtract(b1, 5.5m)));
+            Assert.AreEqual(96.965m, Calc.Sum(Calc.Product(b1, 5.5m)));
+            Assert.AreEqual(3.2054545454545454545454545454m, Calc.Sum(Calc.Divide(b1, 5.5m)));
+
+
+        }
+
+        [Test]
+        public void DecimalPerformance()
+        {
+            DateTime start = DateTime.Now;
+            const int SIZE = 250;
+            const int REPS = 250;
+            const double EXPECT = .5;
+            decimal[] a = new decimal[SIZE];
+            decimal[] b = new decimal[SIZE];
+            decimal[] c = new decimal[SIZE];
+            for (int i = 0; i < REPS; i++)
+            {
+                init(ref a);
+                init(ref b);
+                c = Calc.Add(a, b);
+                c = Calc.Subtract(a,b);
+                c = Calc.Product(a,b);
+                c = Calc.Divide(a,b);
+                Calc.StdDev(a);
+            }
+
+            double time = DateTime.Now.Subtract(start).TotalSeconds;
+            Assert.LessOrEqual(time, EXPECT);
+            Console.WriteLine("DecimalPerformance (expected): " + time.ToString("N2") + "s (" + EXPECT.ToString("N2") + "), reps/sec: " + (REPS / time).ToString("N0"));
+        }
+
+        [Test]
+        public void IntPerformance()
+        {
+            DateTime start = DateTime.Now;
+            const int SIZE = 500;
+            const int REPS = 500;
+            const double EXPECT = .25;
+            int[] a = new int[SIZE];
+            int[] b = new int[SIZE];
+            int[] c = new int[SIZE];
+            decimal[] d = new decimal[SIZE];
+            for (int i = 0; i < REPS; i++)
+            {
+                init(ref a);
+                init(ref b);
+                c = Calc.Add(a, b);
+                c = Calc.Subtract(a, b);
+                c = Calc.Product(a, b);
+                d = Calc.Divide(a, b);
+                Calc.StdDev(a);
+            }
+
+            double time = DateTime.Now.Subtract(start).TotalSeconds;
+            Assert.LessOrEqual(time, EXPECT);
+            Console.WriteLine("IntPerformance (expected): " + time.ToString("N2") + "s (" + EXPECT.ToString("N2") + "), reps/sec: " + (REPS / time).ToString("N0"));
+        }
+
+        void init(ref int[] a)
+        {
+            Random r = new Random((int)DateTime.Now.Ticks);
+            for (int i = 0; i < a.Length; i++)
+                a[i] = r.Next(0,1000);
+        }
+
+        void init(ref decimal[] a)
+        {
+            Random r = new Random((int)DateTime.Now.Ticks);
+            for (int i = 0; i < a.Length; i++)
+                a[i] = (decimal)r.NextDouble()*1000;
+        }
+
     }
 }
