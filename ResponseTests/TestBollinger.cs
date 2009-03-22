7528984672dd9ae66a157e589e2348dba132e528
frantaparallel@gmail.com <frantaparallel@gmail.com@ccc6a032-7b47-0410-9fd8-1f29c74ce1d2>
2009-03-22 07:51:34 +0000

    issue #181 re-completd... forgot to add a file, plus I moved response tests to own project so there is no nuint requirement for users now.
    git-svn-id: http://tradelink.googlecode.com/svn/trunk@1089 ccc6a032-7b47-0410-9fd8-1f29c74ce1d2

---
+++ ResponseTests/TestBollinger.cs
@@ -0,0 +1,164 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TradeLink.Common;
+using NUnit.Framework;
+using TradeLink.API;
+
+
+namespace Responses
+{
+    [TestFixture]
+    public class TestBollinger
+    {
+        public TestBollinger()
+        {
+        }
+        const string s = "TST";
+        const string x = "NYS";
+        const int d = 20070917;
+        const int t = 92900;
+
+        // we test 2 standard deviations here
+
+
+        TickImpl[] ticklist = new TickImpl[] {
+            TickImpl.NewTrade(s,d,t,1,100,x),
+            TickImpl.NewTrade(s,d,t+110,2,100,x),
+            TickImpl.NewTrade(s,d,t+200,3,100,x),
+            TickImpl.NewTrade(s,d,t+300,4,100,x),
+            TickImpl.NewTrade(s,d,t+400,5,100,x),
+        };
+
+        [Test]
+        public void BollingerTestticklookbacks()
+        {
+            Bollinger bbbb = new Bollinger(2, 4);
+            foreach (TickImpl t in ticklist)
+            {
+                bbbb.newTick(t);
+            }
+
+
+            Assert.That(bbbb.Mean == 3.5M, bbbb.Mean.ToString());
+            Assert.That(bbbb.Devavg == 1.25M, bbbb.Devavg.ToString());
+            Assert.That(bbbb.Sd == 1.1180339887498948482045868343656, bbbb.Sd.ToString());
+            Assert.That(bbbb.Upperband == 5.73606797749978M, bbbb.Upperband.ToString());
+            Assert.That(bbbb.Lowerband == 1.26393202250022M, bbbb.Lowerband.ToString());
+        }
+        
+        
+        
+        [Test]
+        public void BollingerTesttick()
+        {
+            Bollinger b = new Bollinger(2, 10);
+
+            foreach (TickImpl t in ticklist)
+            {
+                b.newTick(t);
+            }
+
+
+            Assert.That(b.Mean == 3, b.Mean.ToString());
+            Assert.That(b.Devavg == 2, b.Devavg.ToString());
+            Assert.That(b.Sd == 1.4142135623730950488016887242097, b.Sd.ToString());
+            Assert.That(b.Upperband == 5.8284271247462M, b.Upperband.ToString());
+            Assert.That(b.Lowerband == 0.1715728752538M, b.Lowerband.ToString());
+        }
+
+
+        [Test]
+        public void BollingerTestticknoargs()
+        {
+            Bollinger bb = new Bollinger();
+            foreach (TickImpl t in ticklist)
+            {
+                bb.newTick(t);
+            }
+
+
+            Assert.That(bb.Mean == 3, bb.Mean.ToString());
+            Assert.That(bb.Devavg == 2, bb.Devavg.ToString());
+            Assert.That(bb.Sd == 1.4142135623730950488016887242097, bb.Sd.ToString());
+            Assert.That(bb.Upperband == 5.8284271247462M, bb.Upperband.ToString());
+            Assert.That(bb.Lowerband == 0.1715728752538M, bb.Lowerband.ToString());
+        }
+
+        [Test]
+
+        public void BollingerBarlisttest()
+        {
+
+            string symbol = "TST";
+
+
+            BarListImpl bl = new BarListImpl(BarInterval.Minute, symbol);
+            Bollinger bbb = new Bollinger(2, BarInterval.Minute, 10);
+            foreach (TickImpl t in ticklist)
+            {
+                bl.AddTick(t);
+                bbb.newBar(bl);
+            }
+
+            Assert.AreEqual(3, bbb.Mean);
+            Assert.AreEqual(2, bbb.Devavg);
+            Assert.AreEqual(1.4142135623730950488016887242097,bbb.Sd);
+            Assert.AreEqual(5.8284271247462M,bbb.Upperband);
+        }
+        [Test]
+        public void BollingerBarlisttestlookbacks()
+        {
+            string symbol = "TST";
+
+            BarListImpl bl = new BarListImpl(BarInterval.Minute, symbol);
+            Bollinger bbb = new Bollinger(2, BarInterval.Minute, 4);
+            foreach (TickImpl t in ticklist)
+            {
+                bl.AddTick(t);
+                bbb.newBar(bl);
+            }
+
+            Assert.That(bbb.Mean == 3.5M, bbb.Mean.ToString());
+            Assert.That(bbb.Devavg == 1.25M, bbb.Devavg.ToString());
+            Assert.That(bbb.Sd == 1.1180339887498948482045868343656, bbb.Sd.ToString());
+            Assert.That(bbb.Upperband == 5.73606797749978M, bbb.Upperband.ToString());
+            Assert.That(bbb.Lowerband == 1.26393202250022M, bbb.Lowerband.ToString());
+        }
+
+        [Test]
+        public void QuoteOnlyTest()
+        {
+            TickImpl[] timesales = new TickImpl[] {
+                TickImpl.NewBid("TST",100m,100),
+                TickImpl.NewAsk("TST",100.1m,200),
+            };
+
+            Bollinger b = new Bollinger();
+            BarListImpl bl = new BarListImpl(BarInterval.FiveMin, "TST");
+
+            foreach (TickImpl k in timesales)
+            {
+                bl.newTick(k);
+                b.newBar(bl);
+            }
+
+            // average volume should be zero bc
+            // with only quotes we should have no bars to process
+            Assert.That(b.Mean== 0, b.Mean.ToString());
+            Assert.That(!bl.Has(1), bl.ToString());
+        }
+    
+    
+    }
+        
+}
+
+
+
+
+
+
+
+
+
