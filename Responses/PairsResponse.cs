f0cd4a6452feb3a072f37663148f0db342a8294c
frantaparallel@gmail.com <frantaparallel@gmail.com@ccc6a032-7b47-0410-9fd8-1f29c74ce1d2>
2009-05-22 02:36:58 +0000

    issue #249 fixed
    git-svn-id: http://tradelink.googlecode.com/svn/trunk@1215 ccc6a032-7b47-0410-9fd8-1f29c74ce1d2

--- Responses/PairsResponse.cs
+++ Responses/PairsResponse.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using TradeLink.Common;
 using TradeLink.API;
+using System.ComponentModel;
 
 namespace Responses
 {
@@ -10,18 +11,19 @@ namespace Responses
     {
         PairsTracker pairs;
         PositionTracker pt = new PositionTracker();
-        public PairsResponse()
+        public PairsResponse() : this(false) { }
+        public PairsResponse(bool auto)
         {
             Name = "PairsResponse";
             isValid = true;
-            pairs = new PairsTracker("WAG", "FRX", .78m, 10);
-            pairs.SpreadOutsideBounds += new DecimalDelegate(pairs_SpreadOutsideBounds);
+            // get parameters from user if we're not running in auto
+            ParamPrompt.Popup(this, auto);
         }
 
-        void pairs_SpreadOutsideBounds(decimal spread)
+        void pairs_SpreadOutsideBounds(decimal difference)
         {
             // b is relatively expensive here
-            if (spread>0)
+            if (difference>0)
             {
               sendorder(new BuyMarket(pairs.Asym, 100));
               sendorder(new SellMarket(pairs.Bsym, 100));
@@ -32,22 +34,50 @@ namespace Responses
                 sendorder(new SellMarket(pairs.Asym, 100));
             }
         }
-        public void GotTick(Tick tick)
+
+        // defaults
+        string _syma = "WAG";
+        string _symb = "FRX";
+        int _spread = 10;
+        decimal _ratio = .78m;
+        int _loss = -100;
+        int _profit = 200;
+        // user parameters that are displayed/fetched when ParamPrompt is called
+        [Description("first symbol")]
+        public string Asym { get { return _syma; } set { _syma = value; } }
+        [Description("second symbol")]
+        public string Bsym { get { return _symb; } set { _symb = value; } }
+        [Description("when delta between a&b exceeds this amount in basis points, enter")]
+        public int BasisPointEntry { get { return _spread; } set { _spread = value; } }
+        [Description("multiply this value times a to get b-relative price")]
+        public decimal RatioA2B { get { return _ratio; } set { _ratio = value; } }
+        [Description("pl loss target")]
+        public int PlLoss { get { return _loss; } set { _loss = value; } }
+        [Description("pl profit target")]
+        public int PlProfit { get { return _profit; } set { _profit = value; } }
+
+        public override void GotTick(Tick tick)
         {
+            // create pairs tracker if not already
+            if (pairs == null)
+            {
+                pairs = new PairsTracker(Asym, Bsym, RatioA2B, BasisPointEntry);
+                pairs.SpreadOutsideBounds += new DecimalDelegate(pairs_SpreadOutsideBounds);
+            }
             pairs.GotTick(tick);
         }
-        public void GotOrder(Order order)
+        public override void GotOrder(Order order)
         {
             senddebug("order: "+order.ToString());
         }
 
 
-        public void GotFill(Trade fill)
+        public override void GotFill(Trade fill)
         {
             D("fill: "+fill.ToString());
             pt.Adjust(fill);
             decimal openpl = Calc.OpenPL(pairs.Aprice, pt[pairs.Asym]) + Calc.OpenPL(pairs.Bprice, pt[pairs.Bsym]);
-            if ((openpl > 200) || (openpl< -100))
+            if ((openpl > _profit) || (openpl< _loss))
                 shutdown();
         }
         void shutdown()
@@ -57,14 +87,19 @@ namespace Responses
             sendorder(new MarketOrderFlat(pt[pairs.Bsym]));
             isValid= false;
         }
-        public void GotOrderCancel(uint cancelid)
+        public override void GotOrderCancel(uint cancelid)
         {
             senddebug("canceled: " + cancelid);
         }
-        public void GotPosition(Position p) 
+        public override void GotPosition(Position p) 
         {
             pt.Adjust(p);
         }
 
+    }
+
+    public class PairsResponseAuto : PairsResponse
+    {
+        public PairsResponseAuto() : base(true) { }
     }
 }
