f4d4692939ac8bfc8039fa4560c0bfbcfc9bacf1
frantaparallel@gmail.com <frantaparallel@gmail.com@ccc6a032-7b47-0410-9fd8-1f29c74ce1d2>
2009-06-16 00:09:40 +0000

    issue #265 started
    git-svn-id: http://tradelink.googlecode.com/svn/trunk@1244 ccc6a032-7b47-0410-9fd8-1f29c74ce1d2

---
+++ ServerIB/ServerIB.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TradeLink.Common;
+using TWSLib;
+
+namespace ServerIB
+{
+    public class ServerIB : TLServer_WM
+    {
+        TWSLib.TwsClass tws1 = new TwsClass();
+        public ServerIB()
+        {
+            tws1.nextValidId += new _DTwsEvents_nextValidIdEventHandler(tws1_nextValidId);
+        }
+
+        void tws1_nextValidId(int id)
+        {
+            
+        }
+    }
+}
