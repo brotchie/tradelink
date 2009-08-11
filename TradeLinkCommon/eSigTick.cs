a8c8f314efa2136e91d0e09a1b5a6353f96dff9a
frantaparallel@gmail.com <frantaparallel@gmail.com@ccc6a032-7b47-0410-9fd8-1f29c74ce1d2>
2009-08-11 02:41:11 +0000

    309 fixed
    git-svn-id: http://tradelink.googlecode.com/svn/trunk@1310 ccc6a032-7b47-0410-9fd8-1f29c74ce1d2

--- TradeLinkCommon/eSigTick.cs
+++ TradeLinkCommon/eSigTick.cs
@@ -33,6 +33,8 @@ namespace TradeLink.Common
             }
             catch (Exception) { return t; }
             if (line == null) return t; // blank line
+            System.Globalization.NumberFormatInfo ni = (System.Globalization.NumberFormatInfo)System.Globalization.NumberFormatInfo.CurrentInfo.Clone();
+            ni.NumberDecimalSeparator = ".";
             string[] r = line.Split(',');
             if (r.Length < 6) return t;
             decimal td = 0;
