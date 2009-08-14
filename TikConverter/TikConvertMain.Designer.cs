0bbb7fcc1027eadec6ae08f89305c75de99577e8
frantaparallel@gmail.com <frantaparallel@gmail.com@ccc6a032-7b47-0410-9fd8-1f29c74ce1d2>
2009-08-14 18:53:04 +0000

    309 work (in progress)
    git-svn-id: http://tradelink.googlecode.com/svn/trunk@1328 ccc6a032-7b47-0410-9fd8-1f29c74ce1d2

---
+++ TikConverter/TikConvertMain.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace CQG2EPF
+{
+    partial class TikConvertMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TikConvertMain));
+            this._msg = new System.Windows.Forms.ListBox();
+            this._inputbut = new System.Windows.Forms.Button();
+            this._defaultsize = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this._progress = new System.Windows.Forms.ToolStripProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this._defaultsize)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // _msg
+            // 
+            this._msg.FormattingEnabled = true;
+            this._msg.ItemHeight = 20;
+            this._msg.Location = new System.Drawing.Point(31, 67);
+            this._msg.Name = "_msg";
+            this._msg.Size = new System.Drawing.Size(315, 144);
+            this._msg.TabIndex = 0;
+            // 
+            // _inputbut
+            // 
+            this._inputbut.Location = new System.Drawing.Point(31, 23);
+            this._inputbut.Name = "_inputbut";
+            this._inputbut.Size = new System.Drawing.Size(144, 36);
+            this._inputbut.TabIndex = 1;
+            this._inputbut.Text = "select input files";
+            this._inputbut.UseVisualStyleBackColor = true;
+            this._inputbut.Click += new System.EventHandler(this._inputbut_Click);
+            // 
+            // _defaultsize
+            // 
+            this._defaultsize.Location = new System.Drawing.Point(237, 35);
+            this._defaultsize.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this._defaultsize.Name = "_defaultsize";
+            this._defaultsize.Size = new System.Drawing.Size(82, 26);
+            this._defaultsize.TabIndex = 2;
+            this._defaultsize.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this._defaultsize.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(209, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Default TradeSize";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this._progress});
+            this.statusStrip1.LayoutStyle = System.Windows.Forms.ToolStripLayoutStyle.Flow;
+            this.statusStrip1.Location = new System.Drawing.Point(0, 226);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(376, 22);
+            this.statusStrip1.SizingGrip = false;
+            this.statusStrip1.TabIndex = 4;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // _progress
+            // 
+            this._progress.Name = "_progress";
+            this._progress.Size = new System.Drawing.Size(325, 16);
+            // 
+            // CQG2EPFMAIN
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(376, 248);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this._defaultsize);
+            this.Controls.Add(this._inputbut);
+            this.Controls.Add(this._msg);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "CQG2EPFMAIN";
+            this.Text = "CQG2EPF";
+            ((System.ComponentModel.ISupportInitialize)(this._defaultsize)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox _msg;
+        private System.Windows.Forms.Button _inputbut;
+        private System.Windows.Forms.NumericUpDown _defaultsize;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripProgressBar _progress;
+    }
+}
+
---
+++ TikConverter/TikConvertMain.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using TradeLink.Common;
+using TradeLink.API;
+using System.ComponentModel;
+
+namespace CQG2EPF
+{
+    public partial class TikConvertMain : Form
+    {
+        BackgroundWorker bw = new BackgroundWorker();
+        public TikConvertMain()
+        {
+            InitializeComponent();
+            bw.WorkerReportsProgress = true;
+            bw.DoWork += new DoWorkEventHandler(bw_DoWork);
+            bw.RunWorkerCompleted += new RunWorkerCompletedEventHandler(bw_RunWorkerCompleted);
+        }
+
+
+
+
+        Dictionary<string, string> _filesyms = new Dictionary<string, string>();
+        string _path = string.Empty;
+        private void _inputbut_Click(object sender, EventArgs e)
+        {
+            // make sure we only convert one group at a time
+            if (bw.IsBusy) { debug("wait until conversion completes..."); return; }
+            OpenFileDialog of = new OpenFileDialog();
+            // allow selection of multiple inputs
+            of.Multiselect = true;
+            // keep track of bytes so we can approximate progress
+            long bytes = 0;
+            if (of.ShowDialog() == DialogResult.OK)
+            {
+                bool g = true;
+                foreach (string file in of.FileNames)
+                {
+                    _path = Path.GetDirectoryName(file);
+                    // get size of current file and append to total size
+                    FileInfo fi = new FileInfo(file);
+                    bytes += fi.Length;
+                }
+                // estimate total ticks
+                _approxtotal = (int)((double)bytes / 51);
+                // reset progress bar
+                progress(0);
+                // start background thread to convert
+                bw.RunWorkerAsync(of.FileNames);
+                debug("started convesion");
+
+            }
+        }
+
+        void bw_DoWork(object sender, DoWorkEventArgs e)
+        {
+            string[] filenames = (string[])e.Argument;
+            bool g = e.Result != null ? (bool)e.Result : true;
+            foreach (string file in filenames)
+            {
+                debug("input file: " + Path.GetFileNameWithoutExtension(file));
+                // convert file
+                bool fg = convert(file, (int)_defaultsize.Value);
+                // report progress
+                if (!fg) debug("error converting file: " + file);
+                g &= fg;
+            }
+            e.Result = g;
+
+        }
+
+        void bw_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            // status back to user
+            bool g = (bool)e.Result;
+            debug("processed ticks: " + _ticksprocessed.ToString("N0"));
+            if (g) debug("converted files successfully.");
+            else debug("errors converting files.");
+            // reset progress bar
+            progress(0);
+            // reset ticks processed
+            _ticksprocessed = 0;
+        }
+
+        int _ticksprocessed = 0;
+        int _approxtotal = 0;
+        bool convert(string filename,int tradesize)
+        {
+            bool g = true;
+            // get output filename
+            string convertname = string.Empty;
+            // setup writing to output
+            TikWriter outfile = null;
+            // setup input file
+            StreamReader infile = null;
+            try
+            {
+                // open input file
+                infile = new StreamReader(filename);
+            }
+            catch (Exception ex) { debug("error reading input header:" + ex.Message); g = false; }
+            // setup previous tick
+            TickImpl pk = new TickImpl();
+            do
+            {
+                // get next tick from the file
+                TickImpl k = parseline(infile.ReadLine(), tradesize);
+                // if dates don't match, we need to write new output file
+                if (k.date != pk.date)
+                {
+                    // if the outfile was open previously, close it
+                    if (outfile != null) outfile.Close();
+                    try
+                    {
+                        // get file name
+                        string fn = _path+"//"+k.symbol + k.date + ".EPF";
+                        // setup new file
+                        outfile = new TikWriter(k.symbol, k.date);
+                        // report progress
+                        progress((double)_ticksprocessed / _approxtotal);
+                    }
+                    catch (Exception ex) { debug(ex.Message); g = false; }
+                }
+                try
+                {
+                    // write the tick
+                    outfile.newTick(k);
+                }
+                catch (Exception ex) { debug("error writing output tick: " + ex.Message); g = false; }
+                // save this tick as previous tick
+                pk = k;
+                // count the tick as processed
+                _ticksprocessed++;
+            }
+            // keep going until input file is exhausted
+            while (!infile.EndOfStream);
+            // close output file
+            outfile.Close();
+            // return status
+            return g;
+        }
+
+        // fields of tradestation files
+        const int SYM = 0;
+        const int DATE = 1;
+        const int TIME = 3;
+        const int TRADE = 4;
+
+        // here is where a line is converted
+        TickImpl parseline(string line, int defaultsize)
+        {
+            // split line
+            string[] r = line.Split(',');
+            // create tick for this symbol
+            TickImpl k = new TickImpl(r[SYM]);
+            // setup temp vars
+            int iv = 0;
+            decimal dv = 0;
+            // parse date
+            if (int.TryParse(r[DATE], out iv))
+                k.date = iv;
+            // parse time
+            if (int.TryParse(r[TIME], out iv))
+                k.time = iv * 100;
+            // parse close as trade price
+            if (decimal.TryParse(r[TRADE], out dv))
+            {
+                k.trade = (decimal)dv/100;
+                k.size = defaultsize;
+            }
+            // return tick
+            return k;
+        }
+        delegate void pdouble(double p);
+        void progress(double percent)
+        {
+            int p = (int)(percent * 100);
+            if (p > 100) p = 100;
+            if (p < 0) p = 0;
+            // if being called from a background thread, 
+            // invoke UI thread to update screen
+            if (statusStrip1.InvokeRequired)
+                statusStrip1.Invoke(new pdouble(progress), new object[] { percent });
+            else
+            {
+                _progress.Value = p;
+                _progress.Invalidate();
+            }
+        }
+
+        void debug(string msg)
+        {
+            if (InvokeRequired)
+                Invoke(new DebugDelegate(debug), new object[] { msg });
+            else
+            {
+                msg = DateTime.Now.ToShortTimeString() + " " + msg;
+                _msg.Items.Add(msg);
+                _msg.SelectedIndex = _msg.Items.Count - 1;
+            }
+
+        }
+    }
+}
