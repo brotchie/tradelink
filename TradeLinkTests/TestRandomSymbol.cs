63a3765112edf8ca832165a8aed785004c2476f0
frantaparallel@gmail.com <frantaparallel@gmail.com@ccc6a032-7b47-0410-9fd8-1f29c74ce1d2>
2009-08-21 19:45:47 +0000

    319 work
    git-svn-id: http://tradelink.googlecode.com/svn/trunk@1369 ccc6a032-7b47-0410-9fd8-1f29c74ce1d2

---
+++ TradeLinkTests/TestRandomSymbol.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using TradeLink.Common;
+using TradeLink.API;
+using TradeLink.Research;
+
+namespace TestTradeLink
+{
+    [TestFixture]
+    public class TestRandomSymbol
+    {
+        public TestRandomSymbol() { }
+
+        [Test]
+        public void Basics()
+        {
+            string [] syms = RandomSymbol.GetSymbols((int)DateTime.Now.Ticks, 4, 100);
+            bool v = true;
+
+            foreach (string sym in syms)
+            {
+                bool bv = v;
+                v &= (sym.Length > 0) && (System.Text.RegularExpressions.Regex.Replace(sym, "[a-z]", "", System.Text.RegularExpressions.RegexOptions.IgnoreCase).Length == 0);
+            }
+            Assert.IsTrue(v);
+        }
+    }
+}
---
+++ TradeLinkTests/TestRandomTicks.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using TradeLink.API;
+using TradeLink.Common;
+using TradeLink.Research;
+using NUnit.Framework;
+
+namespace TestTradeLink
+{
+    [TestFixture]
+    public class TestRandomTicks
+    {
+        public TestRandomTicks()
+        {
+        }
+
+        [Test]
+        public void Basics()
+        {
+            Tick[] ticks = RandomTicks.GenerateSymbol("TST", 1000);
+            bool v = true;
+            foreach (Tick k in ticks)
+                v &= k.isValid;
+            Assert.IsTrue(v);
+        }
+    }
+}
