eed0f76da503408f6e18f65058fd741856824daf
frantaparallel@gmail.com <frantaparallel@gmail.com@ccc6a032-7b47-0410-9fd8-1f29c74ce1d2>
2009-10-09 21:35:18 +0000

    #371 verified
    git-svn-id: http://tradelink.googlecode.com/svn/trunk@1481 ccc6a032-7b47-0410-9fd8-1f29c74ce1d2

---
+++ TradeLinkTests/TestTLFastInterop.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using TradeLink.Common;
+using TradeLink.API;
+using TradeLink.Research;
+using System.Runtime.InteropServices;
+
+namespace TestTradeLink
+{
+    [TestFixture(Description="May not work on machines without brokerserver installed to default location.")]
+    public class TestTLFastInterop
+    {
+        [DllImport("c:\\program files\\TradeLink\\BrokerServer\\TradeLibFast.dll",EntryPoint="TLSENDORDER")]
+        public static extern int SendOrder(string sym, bool side, int size, double limit, double stop, int id, string account, string dest);
+        
+
+        [Test]
+        public void OrderPrecision()
+        {
+            // prepare random
+            Random r = new Random((int)DateTime.Now.Ticks);
+            // get a price
+            double p = r.NextDouble() * 1000000 / 100;
+            // send the order
+            int err = SendOrder("TST", true, 300, p, 0, 1, string.Empty, string.Empty);
+            // make sure it was received
+            Assert.AreEqual((int)MessageTypes.OK, err);
+            // verify price to 5 decimal places
+            double expect = Math.Round(p, 5);
+            double actual = (double)Math.Round(last.price, 5);
+            Assert.AreEqual(expect, actual );
+
+        }
+
+        [Test]
+        public void InterOpSpeed()
+        {
+            const int TESTSIZE = 100;
+            // get some random prices
+            Tick[] data = RandomTicks.GenerateSymbol(sym, TESTSIZE);
+            // track the time
+            System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+            int bad = 0;
+            sw.Start();
+            for (int i = 0; i < data.Length; i++)
+            {
+                int err = SendOrder(sym, true, 300, (double)data[i].trade, 0, 1, "TESTACCOUNT", "TESTDEST");
+                if (err!=0) bad++;
+            }
+            sw.Stop();
+            long elapms = sw.ElapsedMilliseconds;
+            double elap = (double)elapms/1000;
+            double rate = TESTSIZE / elap;
+            Console.WriteLine("InterOpSpeed elap: " + elap.ToString("N1") + " rate: " + rate.ToString("N0") + " orders/sec");
+            // make sure no bad orders
+            Assert.AreEqual(0,bad);
+            // make sure fast
+            Assert.LessOrEqual(elap, .5);
+        }
+
+        Order last = new OrderImpl();
+
+        void tl_newSendOrderRequest(Order o)
+        {
+            last = o;
+  
+        }
+        const string sym = "TST";
+        public TestTLFastInterop()
+        {
+            // start a server
+            TLServer_WM tl = new TLServer_WM();
+            // handle new orders
+            tl.newSendOrderRequest += new OrderDelegate(tl_newSendOrderRequest);
+
+        }
+    }
+
+
+}
---
+++ TradeLinkTests/TestTemplate.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using TradeLink.Common;
+using TradeLink.API;
+using TradeLink.Research;
+
+namespace TestTradeLink
+{
+    [TestFixture]
+    public class TestTemplate
+    {
+
+
+        [Test]
+        public void Test()
+        {
+
+        }
+    }
+}
