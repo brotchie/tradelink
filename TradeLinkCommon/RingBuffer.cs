968b2589d146a0dbbd51ee31acb716df4e4b5702
frantaparallel@gmail.com <frantaparallel@gmail.com@ccc6a032-7b47-0410-9fd8-1f29c74ce1d2>
2009-12-19 07:31:48 +0000

    468
    git-svn-id: http://tradelink.googlecode.com/svn/trunk@1681 ccc6a032-7b47-0410-9fd8-1f29c74ce1d2

---
+++ TradeLinkCommon/RingBuffer.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+
+namespace TradeLink.Common
+{
+    /// <summary>
+    /// Circular buffer.   Allows reading and writing of up to BufferSize 
+    /// element count in a FIFO fashion.   Useful for passing data asynchronously 
+    /// between threads without requiring locking.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public struct RingBuffer<T>
+    {
+        T[] _buffer;
+        volatile int _rc;
+        volatile int _wc;
+        volatile bool _flipped;
+        /// <summary>
+        /// maximum # of unread elements the buffer can hold.
+        /// </summary>
+        public int BufferSize { get { return _buffer.Length; } }
+        /// <summary>
+        /// Count of unread elements
+        /// </summary>
+        public int Count { get { return System.Math.Abs(_wc - _rc); } }
+        /// <summary>
+        /// writes a value into the buffer
+        /// </summary>
+        /// <param name="val"></param>
+        public void Write(T val)
+        {
+            _buffer[_wc++] = val;
+            if (_wc >= _buffer.Length)
+            {
+                _wc = 0;
+                _flipped = true;
+            }
+        }
+        /// <summary>
+        /// returns true if all written elements have been read from buffer
+        /// </summary>
+        public bool isEmpty { get { return (_rc >= _wc) && !_flipped; } }
+        /// <summary>
+        /// reads next unread element from buffer
+        /// </summary>
+        /// <returns></returns>
+        public T Read()
+        {
+            T val = _buffer[_rc++];
+            if (_rc >= _buffer.Length)
+            {
+                _rc = 0;
+                _flipped = false;
+            }
+            return val;
+        }
+        /// <summary>
+        /// create a buffer of fixed size in elements
+        /// </summary>
+        /// <param name="BufferSize"></param>
+        public RingBuffer(int BufferSize)
+        {
+            _buffer = new T[BufferSize];
+            _wc = 0;
+            _rc = 0;
+            _flipped = false;
+        }
+    }
+}
