a8641084aa5ceb7547b49fd73c8e983e806e2fc1
frantaparallel@gmail.com <frantaparallel@gmail.com@ccc6a032-7b47-0410-9fd8-1f29c74ce1d2>
2010-02-13 07:14:50 +0000

    574
    git-svn-id: http://tradelink.googlecode.com/svn/trunk@1890 ccc6a032-7b47-0410-9fd8-1f29c74ce1d2

--- ResponseTests/runtests.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-
-for /F "delims=;" %%i in ('dir /b /x c:\progra~1\NUnit*') do set n_dir=%%i
-set NUNIT=c:\progra~1\%n_dir%
-"%NUNIT%\bin\nunit-console.exe" "%~f2" /config:%1
-"%NUNIT%\bin\net-2.0\nunit-console.exe" "%~f2" /config:%1
