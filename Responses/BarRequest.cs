2e24d48d4afa6dfa1511bd5e7b47a4ab3edaddf0
frantaparallel@gmail.com <frantaparallel@gmail.com@ccc6a032-7b47-0410-9fd8-1f29c74ce1d2>
2010-02-20 03:33:31 +0000

    596 work
    git-svn-id: http://tradelink.googlecode.com/svn/trunk@1905 ccc6a032-7b47-0410-9fd8-1f29c74ce1d2

---
+++ Responses/BarRequest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TradeLink.API;
+using TradeLink.Common;
+
+namespace Responses
+{
+    /// <summary>
+    /// demonstrates response that uses historical bar data
+    /// </summary>
+    public class BarRequest : ResponseTemplate
+    {
+        MessageTracker _mt = new MessageTracker();
+        BarListTracker _blt = new BarListTracker();
+
+        public override void GotTick(Tick k)
+        {
+            // if we don't have bar data, request historical data
+            if (_blt[k.symbol].Count == 0)
+            {
+                sendmessage(MessageTypes.BARREQUEST, BarImpl.BuildBarRequest(k.symbol, BarInterval.Hour));
+            }
+            // update whatever data we have with ticks
+            _blt.newTick(k);
+
+        }
+
+        public override void GotMessage(MessageTypes type, uint source, uint dest, uint msgid, string request, ref string response)
+        {
+            _mt.GotMessage(type, source, dest, msgid, request, ref response);
+        }
+
+    }
+}
