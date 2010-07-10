f35e37111435d395cc27be249dd65742a9abc42a
frantaparallel@gmail.com <frantaparallel@gmail.com@ccc6a032-7b47-0410-9fd8-1f29c74ce1d2>
2010-07-10 04:10:16 +0000

    845
    git-svn-id: http://tradelink.googlecode.com/svn/trunk@2330 ccc6a032-7b47-0410-9fd8-1f29c74ce1d2

--- TradeLinkAppKit/Log.cs
+++ TradeLinkAppKit/Log.cs
@@ -18,12 +18,32 @@ namespace TradeLink.AppKit
         StreamWriter _log = null;
         public bool isEnabled = true;
         private string fn = string.Empty;
+        /// <summary>
+        /// full path of log file
+        /// </summary>
         public string FullName { get { return fn; } }
         private StringBuilder _content = new StringBuilder();
+        /// <summary>
+        /// contents of log file
+        /// </summary>
         public string Content  { get { return _content.ToString(); } }
-        public Log(string program) : this(program, true, true, Util.ProgramData(program)) { }
-        public Log(string logname, bool dateinlogname,bool appendtolog,string path)
+        /// <summary>
+        /// create a log
+        /// </summary>
+        /// <param name="program"></param>
+        public Log(string program) : this(program, true, true, Util.ProgramData(program),true) { }
+        bool _timestamps = true;
+        /// <summary>
+        /// create a log
+        /// </summary>
+        /// <param name="logname"></param>
+        /// <param name="dateinlogname"></param>
+        /// <param name="appendtolog"></param>
+        /// <param name="path"></param>
+        /// <param name="timestamps"></param>
+        public Log(string logname, bool dateinlogname,bool appendtolog,string path, bool timestamps)
         {
+            _timestamps = timestamps;
             fn = path+"\\"+logname+(dateinlogname ? "."+TradeLink.Common.Util.ToTLDate(DateTime.Now): "") + ".txt";
             try
             {
@@ -33,7 +53,10 @@ namespace TradeLink.AppKit
             catch (Exception ex) { _log = null; }
         }
         public event DebugFullDelegate SendDebug;
-
+        /// <summary>
+        /// log something
+        /// </summary>
+        /// <param name="msg"></param>
         public void GotDebug(Debug msg) 
         {
             if (SendDebug != null)
@@ -43,17 +66,30 @@ namespace TradeLink.AppKit
             {
                 if (_log != null)
                 {
-                    string data = DateTime.Now.ToString("HHmmss") + ": " + msg.Msg;
-                    _log.WriteLine(data);
-                    _content.AppendLine(data);
+                    StringBuilder sb = new StringBuilder();
+                    if (_timestamps)
+                    {
+                        sb.Append(DateTime.Now.ToString("HHmmss"));
+                        sb.Append(": ");
+                    }
+                    sb.Append(msg.Msg);
+                    _log.WriteLine(sb.ToString());
+                    _content.Append(sb.ToString());
                 }
             }
             catch { }
         }
+        /// <summary>
+        /// log something
+        /// </summary>
+        /// <param name="msg"></param>
         public void GotDebug(string msg)
         {
             GotDebug(DebugImpl.Create(msg));
         }
+        /// <summary>
+        /// close the log
+        /// </summary>
         public void Stop()
         {
             try
