1ad184589deb862c0cd6516ad726b639a1732b95
frantaparallel@gmail.com <frantaparallel@gmail.com@ccc6a032-7b47-0410-9fd8-1f29c74ce1d2>
2010-09-04 11:20:18 +0000

    988
    git-svn-id: http://tradelink.googlecode.com/svn/trunk@2482 ccc6a032-7b47-0410-9fd8-1f29c74ce1d2

---
+++ TradeLinkAppKit/AuthInfoPrompt.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace TradeLink.AppKit
+{
+    partial class AuthInfoPrompt
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this._un = new System.Windows.Forms.TextBox();
+            this._pw = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this._stat = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "User:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Password:";
+            // 
+            // _un
+            // 
+            this._un.Location = new System.Drawing.Point(111, 55);
+            this._un.Name = "_un";
+            this._un.Size = new System.Drawing.Size(100, 26);
+            this._un.TabIndex = 2;
+            // 
+            // _pw
+            // 
+            this._pw.Location = new System.Drawing.Point(111, 100);
+            this._pw.Name = "_pw";
+            this._pw.PasswordChar = '*';
+            this._pw.Size = new System.Drawing.Size(100, 26);
+            this._pw.TabIndex = 3;
+            this._pw.UseSystemPasswordChar = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(90, 150);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(83, 27);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Accept";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // _stat
+            // 
+            this._stat.AutoSize = true;
+            this._stat.Location = new System.Drawing.Point(13, 13);
+            this._stat.Name = "_stat";
+            this._stat.Size = new System.Drawing.Size(198, 20);
+            this._stat.TabIndex = 5;
+            this._stat.Text = "Enter your login info below.";
+            // 
+            // AuthInfoPrompt
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(291, 187);
+            this.Controls.Add(this._stat);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this._pw);
+            this.Controls.Add(this._un);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "AuthInfoPrompt";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox _un;
+        private System.Windows.Forms.TextBox _pw;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label _stat;
+    }
+}
---
+++ TradeLinkAppKit/AuthInfoPrompt.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using TradeLink.API;
+
+namespace TradeLink.AppKit
+{
+    public partial class AuthInfoPrompt : Form
+    {
+        public delegate void AuthInfoDelegate(TradeLink.AppKit.AuthInfo ai);
+        public event AuthInfoDelegate NewAuthInfo;
+
+        public AuthInfoPrompt() : this(new TradeLink.AppKit.AuthInfo()) { }
+        public AuthInfoPrompt(TradeLink.AppKit.AuthInfo authinfo)
+        {
+            ai = authinfo;
+            InitializeComponent();
+            _un.Text = ai.Username;
+            _pw.Text = ai.Password;
+            Invalidate(true);
+            
+        }
+
+        public void Toggle()
+        {
+            if (Visible)
+                Hide();
+            else
+                Show();
+        }
+
+        TradeLink.AppKit.AuthInfo ai;
+
+        public TradeLink.AppKit.AuthInfo Accepted { get { return ai; } }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ai.Username = _un.Text;
+            ai.Password = _pw.Text;
+            if (NewAuthInfo != null)
+                NewAuthInfo(ai);
+            
+        }
+
+        static DebugDelegate d = null;
+        static AuthInfoPrompt aip;
+        static string PROGRAM = string.Empty;
+        public static void Prompt(string program) { Prompt(program, Auth.GetProgramAuth(program), true, null); }
+        public static void Prompt(string program, AuthInfo ai) { Prompt(program, ai, true, null); }
+        public static void Prompt(string program, bool pause) { Prompt(program, Auth.GetProgramAuth(program), pause, null); }
+        public static void Prompt(string program, AuthInfo ai, bool pause) { Prompt(program, ai, pause, null); }
+        public static void Prompt(string program, AuthInfo ai, bool pause, DebugDelegate deb)
+        {
+            PROGRAM = program;
+            aip = new AuthInfoPrompt(ai);
+            aip.NewAuthInfo += new AuthInfoDelegate(aip_NewAuthInfo);
+            if (pause)
+                aip.ShowDialog();
+            else
+                aip.Show();
+        }
+
+        static void aip_NewAuthInfo(AuthInfo ai)
+        {
+            SetProgramAuth(PROGRAM, aip.Accepted, d);
+        }
+
+        static void debug(string msg)
+        {
+            if (d != null)
+            {
+                d(msg);
+            }
+        }
+
+        public static bool SetProgramAuth(string program, AuthInfo ai) { return SetProgramAuth(program, ai); }
+        public static bool SetProgramAuth(string program, AuthInfo ai, DebugDelegate deb)
+        {
+            return SetAuthInfo(TradeLink.Common.Util.ProgramData(program) + Auth.AuthFile, ai, deb);
+        }
+        public static bool SetAuthInfo(string file, AuthInfo ai) { return SetAuthInfo(file, ai, null); }
+        public static bool SetAuthInfo(string file, AuthInfo ai, DebugDelegate deb)
+        {
+            try
+            {
+                System.IO.StreamWriter sw = new System.IO.StreamWriter(file, false);
+                sw.WriteLine(ai.Username);
+                sw.WriteLine(ai.Password);
+                sw.Close();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                if (deb != null)
+                {
+                    deb("error saving file: "+file+" with: "+ex.Message + ex.StackTrace);
+                }
+            }
+            return false;
+        }
+    }
+
+    public class AIP : AuthInfoPrompt
+    {
+    }
+}
