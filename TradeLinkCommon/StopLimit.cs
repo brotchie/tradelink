509d37fda60f9625905978ab8dbc5aa45e050eb8
frantaparallel@gmail.com <frantaparallel@gmail.com@ccc6a032-7b47-0410-9fd8-1f29c74ce1d2>
2011-01-01 07:39:44 +0000

    1182
    git-svn-id: http://tradelink.googlecode.com/svn/trunk@2707 ccc6a032-7b47-0410-9fd8-1f29c74ce1d2

---
+++ TradeLinkCommon/StopLimit.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TradeLink.Common
+{
+    /// <summary>
+    /// Create stop limit orders.
+    /// </summary>
+    public class StopLimitOrder : OrderImpl
+    {
+        public StopLimitOrder(string sym, bool side, int size, decimal price, decimal stop, long orderid) : base(sym, side, size, price, stop, string.Empty, 0, 0, orderid) { }
+        public StopLimitOrder(string sym, bool side, int size, decimal price, decimal stop, string comment) : base(sym, side, size, price, stop, comment, 0, 0) { }
+        public StopLimitOrder(string sym, bool side, int size, decimal price, decimal stop) : base(sym, side, size, price, stop, "", 0, 0) { }
+    }
+    /// <summary>
+    /// Create buy stop limit orders.
+    /// </summary>
+    public class BuyStopLimit : OrderImpl
+    {
+        public BuyStopLimit(string sym, int size, decimal price, decimal stop, long orderid) : base(sym, true, size, price, stop, string.Empty, 0, 0, orderid) { }
+        public BuyStopLimit(string sym, int size, decimal price, decimal stop, string comment) : base(sym, true, size, price, stop, comment, 0, 0) { }
+        public BuyStopLimit(string sym, int size, decimal price, decimal stop) : base(sym, true, size, price, stop, "", 0, 0) { }
+    }
+
+    /// <summary>
+    /// Create sell-stop limit orders.
+    /// </summary>
+    public class SellStopLimit : OrderImpl
+    {
+        public SellStopLimit(string sym, int size, decimal price, decimal stop, long orderid) : base(sym, false, size, price, stop, string.Empty, 0, 0, orderid) { }
+        public SellStopLimit(string sym, int size, decimal price, decimal stop, string comment) : base(sym, false, size, price, stop, comment, 0, 0) { }
+        public SellStopLimit(string sym, int size, decimal price, decimal stop) : base(sym, false, size, price, stop, "", 0, 0) { }
+    }
+}
