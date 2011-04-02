e6d835734d985edc8afdc35d840c7590bba7219f
frantaparallel@gmail.com <frantaparallel@gmail.com@ccc6a032-7b47-0410-9fd8-1f29c74ce1d2>
2011-04-02 01:21:52 +0000

    1336
    git-svn-id: http://tradelink.googlecode.com/svn/trunk@2899 ccc6a032-7b47-0410-9fd8-1f29c74ce1d2

---
+++ TradeLinkCommon/RejectTracker.cs
@@ -0,0 +1,151 @@
+﻿
+using TradeLink.API;
+
+
+namespace TradeLink.Common
+{
+    public class RejectTracker : GenericTracker<bool>
+    {
+        IdTracker _idt;
+        public RejectTracker() : this(new IdTracker()) { }
+        public RejectTracker(IdTracker idt)
+        {
+            _idt = idt;
+            NewTxt += new TextIdxDelegate(RejectTracker_NewTxt);
+        }
+
+        void RejectTracker_NewTxt(string txt, int idx)
+        {
+            ids.addindex(txt);
+            times.addindex(txt);
+        }
+
+        long gettime { get { return System.DateTime.Now.Ticks; } }
+        
+        public event LongDelegate SendRejectEvent;
+        int _rejectwaitms = 500;
+        public int RejectAfterMs { get { return _rejectwaitms; } set { _rejectwaitms = value; } }
+        public void sendorder(Order o)
+        {
+            // assign an id if we don't have one already
+            if (o.id == 0)
+            {
+                o.id = _idt.AssignId;
+                // notify of new id
+                debug(o.ToString() + " did not have id, assigned id: " + o.id);
+            }
+            // get index
+            int idx = addindex(o.id.ToString(), false);
+            // ensure we are tracking id
+            ids[idx] = o.id;
+            // get time
+            long ctime = gettime;
+            // record time order sent
+            times[idx] = ctime;
+            // notify
+            debug(o.id + " tracking sent time: " + ctime);
+            // pass order through
+            if (SendOrderEvent != null)
+                SendOrderEvent(o);
+            // check rejects
+            checkrejects(ctime);
+        }
+
+        public event DebugDelegate SendDebugEvent;
+
+        void debug(string msg)
+        {
+            if (SendDebugEvent != null)
+                SendDebugEvent(msg);
+        }
+
+        /// <summary>
+        /// if cancels are passed through, consider them acks
+        /// </summary>
+        /// <param name="id"></param>
+        public void GotCancel(long id)
+        {
+            // check other rejections while we're here
+            checkrejects(gettime);
+            // see if we have this order
+            int idx = getindex(id.ToString());
+            // ignore unknown
+            if (idx < 0)
+                return;
+            // notify
+            debug(id + " got cancel ack. (not rejected)");
+            // otherwise ack cancel
+            this[idx] = true;
+        }
+
+        public void checkrejects(long ctime)
+        {
+            // compute allowed delay in nano-seconds that defines a reject
+            long delay = RejectAfterMs*10000;
+            // check every id
+            for (int idx = 0; idx<this.Count; idx++)
+            {
+                // ignore ids which have already been acked (or rejected)
+                if (this[idx])
+                    continue;
+                // otherwise check for delay
+                if ((ctime - times[idx]) > delay)
+                {
+                    // get id
+                    long rid = ids[idx];
+                    // notify
+                    debug(rid + " rejecting at " + ctime + " (" + times[idx] + ").");
+                    // reject
+                    reject(ids[idx]);
+                }
+            }
+            
+        }
+
+        void reject(long id)
+        {
+            // get index
+            int idx = getindex(id.ToString());
+            // ignore unknown
+            if (idx < 0)
+            {
+                debug(id + " could not reject unknown id.");
+                return;
+            }
+            // mark as acked/rejected
+            this[idx] = true;
+            // reject
+            if (SendRejectEvent != null)
+                SendRejectEvent(id);
+        }
+
+        public event OrderDelegate SendOrderEvent;
+
+        public void newTick(Tick k)
+        {
+            checkrejects(gettime);
+        }
+        GenericTracker<long> times = new GenericTracker<long>();
+        GenericTracker<long> ids = new GenericTracker<long>();
+        public void GotOrder(Order o)
+        {
+
+            // get index
+            int idx = getindex(o.id.ToString());
+            // ensure we have this order
+            if (idx < 0)
+            {
+                // check rejects before we quit
+                checkrejects(gettime);
+                // we're done
+                return;
+            }
+            // ack the order
+            this[idx] = true;
+            // check rejects
+            checkrejects(gettime);
+        }
+    }
+
+    
+}
