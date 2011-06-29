3015b469065b7fe6a7d3edfb1034a46b11eb5bc1
frantaparallel@gmail.com <frantaparallel@gmail.com@ccc6a032-7b47-0410-9fd8-1f29c74ce1d2>
2011-06-29 19:57:57 +0000

    1472 work
    git-svn-id: http://tradelink.googlecode.com/svn/trunk@3057 ccc6a032-7b47-0410-9fd8-1f29c74ce1d2

---
+++ TradeLinkCommon/REGSHO_ShortTracker.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using TradeLink.API;
+
+namespace TradeLink.Common
+{
+    public class REGSHO_ShortTracker : OrderTracker
+    {
+        string _acct = string.Empty;
+
+        public string DefaultAccount { get { return _acct; } set { _acct = value; pt.DefaultAccount = _acct; } }
+
+        PositionTracker pt = new PositionTracker();
+
+        public override void GotFill(Trade f)
+        {
+            pt.Adjust(f);
+            base.GotFill(f);
+        }
+        /// <summary>
+        /// returns true if order is a true short
+        /// (versus a sell/buy)
+        /// </summary>
+        /// <param name="ord"></param>
+        /// <returns></returns>
+        public bool isOrderShort(Order ord)
+        {
+            // order is a buy
+            if (ord.side)
+                return false;
+            // get symbol position
+            Position p = pt[ord.symbol];
+            // if we're flat return side
+            if (p.isFlat)
+                return !ord.side;
+            // if same side return side
+            if (p.isLong==ord.side)
+                return ord.side;
+            // get all pending orders for symbol
+            int exitsize = 0;
+            foreach (Order o in this)
+                if (!isPending(o.id))
+                    continue;
+                else if (o.side != p.isLong)
+                    exitsize += o.UnsignedSize;
+            // see if size will completely exit position
+            if (exitsize >= p.UnsignedSize)
+                return true;
+
+            return false;
+        }
+    }
+}
