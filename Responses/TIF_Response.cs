e7d689a0b5593cfb5c36c2df35ac63988672c5dc
josh@pracplay.com <josh@pracplay.com@ccc6a032-7b47-0410-9fd8-1f29c74ce1d2>
2012-02-10 21:53:59 +0000

    1727
    git-svn-id: http://tradelink.googlecode.com/svn/trunk@3299 ccc6a032-7b47-0410-9fd8-1f29c74ce1d2

---
+++ Responses/TIF_Response.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using TradeLink.Common;
+using TradeLink.API;
+
+namespace Responses
+{
+    public class TIF_Response : ResponseTemplate
+    {
+        TIFTracker tt;
+        IdTracker idt;
+        public override void Reset()
+        {
+            idt = new IdTracker(ID);
+            tt = new TIFTracker(idt);
+            tt.SendCancelEvent += new LongDelegate(sendcancel);
+            tt.SendDebugEvent += new DebugDelegate(D);
+            tt.SendOrderEvent += new OrderDelegate(sendorder);
+            tt.DefaultTif = 3;
+
+        }
+
+        int sends = 0;
+
+        public override void GotTick(Tick k)
+        {
+            // enforce tifs
+            tt.newTick(k);
+            // ignore quotes
+            if (!k.isTrade)
+                return;
+            // send entry
+            if (cancels == sends)
+            {
+                Order o = new BuyLimit(k.symbol, 100, k.trade * .7m);
+                tt.SendOrder(o);
+                D(o.symbol + " sent tif-enforced order: " + o);
+                sends++;
+            }
+        }
+
+        int cancels = 0;
+        public override void GotOrderCancel(long id)
+        {
+            cancels++;
+            tt.GotCancel(id);
+            D("tif cancel received: " + id);
+        }
+    }
+}
